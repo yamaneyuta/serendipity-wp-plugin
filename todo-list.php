ec45314cbfce892529dfa4b5e57052517b6f138a
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-08 15:58:34 +0900

    エントリポイントとなるphpファイルから呼び出す処理を有効化

--- todo-list.php
+++ todo-list.php
@@ -19,4 +19,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 // ライブラリ読み込み
 require_once __DIR__ . '/includes/vendor/autoload.php';
 
-// new Cornix\Serendipity\Core\EntryPoint( __FILE__ );
+new Cornix\Serendipity\Core\Hooks\EntryPoint( __FILE__ );
