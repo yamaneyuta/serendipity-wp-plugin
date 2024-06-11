656c03b6a94f1d12750356a73ba6e801170a8e3e
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-11 21:02:56 +0900

    WSL2でも自動的にビルドする設定を追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,5 +13,18 @@ const config = {
 		name: '@storybook/react-webpack5',
 		options: {},
 	},
+	webpackFinal: async (config) => {
+		if(process.env.CHOKIDAR_USEPOLLING === "true") {
+			const poll = process.env.CHOKIDAR_INTERVAL ? Number(process.env.CHOKIDAR_INTERVAL) : 300;
+			config.watchOptions = {
+				...config.watchOptions,
+				poll,
+				aggregateTimeout: 500, // 変更があってから再ビルドするまでの待ち時間
+			};
+		}
+
+		// do mutate the config
+		return config;
+	},
 };
 export default config;
