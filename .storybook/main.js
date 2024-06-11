348336ef1089e574e1f840e6cc85effe9b14f69a
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-11 21:37:17 +0900

    WordPressのCSSをロードする設定を追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,6 +13,17 @@ const config = {
 		name: '@storybook/react-webpack5',
 		options: {},
 	},
+	previewHead: (head) => `
+		${head}
+		<script src="http://localhost:8888/wp-includes/js/dist/vendor/react.js"></script>
+		<!-- <script src="http://localhost:8888/wp-includes/js/dist/vendor/react-dom.js"></script> -->
+
+		<link rel="stylesheet" href="http://localhost:8888/wp-includes/css/dist/edit-post/style.css" media="all">
+		<link rel="stylesheet" href="http://localhost:8888/wp-admin/css/forms.css" media="all">
+		<link rel="stylesheet" href="http://localhost:8888/wp-admin/css/common.css" media="all">
+		<link rel="stylesheet" href="http://localhost:8888/wp-includes/css/dist/components/style.css" media="all">
+		<link rel="stylesheet" href="http://localhost:8888/wp-includes/css/dist/block-editor/style.css" media="all">
+	`,
 	webpackFinal: async (config) => {
 		if(process.env.CHOKIDAR_USEPOLLING === "true") {
 			const poll = process.env.CHOKIDAR_INTERVAL ? Number(process.env.CHOKIDAR_INTERVAL) : 300;
