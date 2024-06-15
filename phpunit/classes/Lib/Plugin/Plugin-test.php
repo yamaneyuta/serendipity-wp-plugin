fc897d0b394f09e3ea098ce0e1bb4cd1845b699c
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-15 18:31:38 +0900

    新しいphpunitで実行できるようにテストソースを修正

--- phpunit/classes/Lib/Plugin/Plugin-test.php
+++ phpunit/classes/Lib/Plugin/Plugin-test.php
@@ -4,16 +4,6 @@ use Cornix\Serendipity\Core\Lib\Plugin\Plugin;
 
 class PluginTest extends WP_UnitTestCase {
 
-	protected function set_up() {
-		parent::set_up();
-		// Your own additional setup.
-	}
-
-	protected function tear_down() {
-		// Your own additional tear down.
-		parent::tear_down();
-	}
-
 	/**
 	 * @test
 	 * @testdox [8A04D33B] プラグインのバージョンを取得する
