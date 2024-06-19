e9d88b8b471a511464220f1ee3370a37e8ccb2bf
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-19 01:01:20 +0000

    GraphQLのスキーマを生成するクラスを追加

---
+++ includes/classes/Lib/GraphQL/PluginSchema.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+namespace Cornix\Serendipity\Core\Lib\GraphQL;
+
+use Cornix\Serendipity\Core\Lib\Path\LocalPath;
+use GraphQL\Language\Parser;
+use GraphQL\Utils\AST;
+use GraphQL\Utils\BuildSchema;
+
+class PluginSchema {
+	public function get() {
+		// キャッシュファイルをこのプラグインディレクトリ内に作成することで
+		// プラグインアップデート時は存在しなくなり、再作成される仕組み。
+		$cache_file_path = LocalPath::get( '/includes/cache/graphql-schema.php' );
+
+		// ここでは、キャッシュファイル生成に失敗しても例外を投げないことで
+		// キャッシュファイルの生成に失敗しても、スキーマの取得ができるようにしている。
+		if ( ! file_exists( $cache_file_path ) ) {
+			$document = Parser::parse( file_get_contents( LocalPath::get( '/includes/assets/schema.graphql' ) ) );
+			// キャッシュファイルを作成
+			file_put_contents( $cache_file_path, "<?php\nreturn " . var_export( AST::toArray( $document ), true ) . ";\n" );
+		} else {
+			$document = AST::fromArray( require $cache_file_path );
+		}
+
+		$schema = BuildSchema::build( $document );
+
+		return $schema;
+	}
+}
