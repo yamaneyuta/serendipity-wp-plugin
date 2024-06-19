f980d1b43aea7df92aa2aa00af24032496f32e91
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-19 21:25:30 +0900

    graphql-codegen定義ファイル

---
+++ codegen.ts
@@ -0,0 +1,24 @@
+import { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+	schema: './includes/assets/graphql/schema.graphql',
+	documents: [ '**/query/*graphql' ],
+	ignoreNoDocuments: true, // for better experience with the watcher
+	generates: {
+		'./src/types/gql/generated.ts': { // `preset: 'client'`を使用しない場合はファイル名を指定
+			// preset: 'client',
+			plugins: [
+				'typescript',
+				'typescript-operations',
+				'typescript-react-query',
+			],
+			// config: {
+			// 	fetcher: './fetcher.ts',
+			// isReactHook: true,
+			// exposeQueryKeys: true,
+			// },
+		}
+	},
+}
+
+export default config
