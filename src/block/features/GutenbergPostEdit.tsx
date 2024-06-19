680ec15bbecd2ff31afd7e1d64501a43151ea4b3
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-19 21:25:52 +0900

    graphqlサンプル実装

--- src/block/features/GutenbergPostEdit.tsx
+++ src/block/features/GutenbergPostEdit.tsx
@@ -1,3 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useEchoQuery } from '../../types/gql/generated';
+
+const client = new QueryClient();
+
 export const GutenbergPostEdit: React.FC = () => {
+	return (
+		<QueryClientProvider client={ client }>
+			<GutenbergPostEditApp />
+		</QueryClientProvider>
+	);
+};
+const GutenbergPostEditApp: React.FC = () => {
+	const { data } = useEchoQuery(
+		{
+			endpoint: 'http://localhost:8888/index.php?rest_route=/todo-list/graphql',
+		},
+		{ message: 'hello typescript client' }
+	);
+
 	return <div>GutenbergPostEdit</div>;
 };
