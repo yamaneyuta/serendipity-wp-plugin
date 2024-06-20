08ab881dd834e8bc0963404e9f8c37ed5fb2b320
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-20 20:20:14 +0900

    lib-frontendに移植したファイルを削除

--- src/lib-frontend/features/global/useGraphQLUrl.ts
+++
@@ -1,9 +0,0 @@
-import { useRestPhpVar } from './_useRestPhpVar';
-
-/**
- * GraphQLのURLを取得します。
- */
-export const useGraphQLUrl = () => {
-	const restPhpVar = useRestPhpVar();
-	return restPhpVar !== null ? restPhpVar.graphqlUrl : null;
-};
--- src/lib-frontend/features/global/useWpRestNonce.ts
+++
@@ -1,9 +0,0 @@
-import { useRestPhpVar } from './_useRestPhpVar';
-
-/**
- * REST APIにアクセスする際のNonceを取得します。
- */
-export const useWpRestNonce = () => {
-	const restPhpVar = useRestPhpVar();
-	return restPhpVar !== null ? restPhpVar.wpRestNonce : null;
-};
