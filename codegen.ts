4b4e94a81cacbf60316e33179f8a4f6472074f75
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-20 18:15:13 +0900

    ファイルを移動graphql/query -> graphql/block

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
 	schema: './includes/assets/graphql/schema.graphql',
-	documents: [ '**/query/*graphql' ],
+	documents: [ '**/block/*graphql' ],
 	ignoreNoDocuments: true, // for better experience with the watcher
 	generates: {
 		'./src/types/gql/generated.ts': { // `preset: 'client'`を使用しない場合はファイル名を指定
