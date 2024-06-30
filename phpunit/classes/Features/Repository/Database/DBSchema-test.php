fb78b1806220b9da556ccbd44b5a4c6e69b91cbc
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-06-30 12:20:15 +0900

    TestWpdbListクラスを作成し、そこからテスト用データベース接続オブジェクト(wpdb)の一覧を取得するように変更

--- phpunit/classes/Features/Repository/Database/DBSchema-test.php
+++ phpunit/classes/Features/Repository/Database/DBSchema-test.php
@@ -108,51 +108,10 @@ class DBSchemaTest extends WP_UnitTestCase {
 	 * @return array<array<wpdb>>
 	 */
 	public function wpdbListProvider(): array {
-		$wp_list = $this->wpdbList();
+		$wp_list = ( new TestWpdbList() )->get();
 		return array_map( fn( wpdb $wpdb ) => array( $wpdb ), $wp_list );
 	}
 
-	/**
-	 *
-	 * @return wpdb[]
-	 */
-	protected function wpdbList(): array {
-		/** @var wpdb[] $ret */
-		$ret = array();
-
-		// `wp-env`が起動した`tests-mysql`に接続するwpdb
-		$ret[] = $GLOBALS['wpdb'];
-
-		// 別途立ち上げたデータベースに接続するwpdb
-		foreach ( $this->extDatabaseHosts() as $host ) {
-			$ret[] = $this->createWpdb( $host );
-		}
-
-		return $ret;
-	}
-
-	private function extDatabaseHosts(): array {
-		// SQL発行テスト用に立ち上げたデータベースのホスト名一覧
-		// ※ `wp-env`が立ち上げた`tests-mysql`は含まない
-		return array(
-			'mysql-phpunit-oldest',
-			'mysql-phpunit-latest',
-			'mariadb-phpunit-oldest',
-			'mariadb-phpunit-latest',
-		);
-	}
-
-	private function createWpdb( string $host ): wpdb {
-		assert( $host !== $GLOBALS['wpdb']->dbhost );
-
-		// phpcsでフォーマットを行うと'WordPress'が'WordPress'に変換されるためphpcs:ignoreを指定
-		$wpdb = new wpdb( 'root', 'password', 'wordpress', $host ); // phpcs:ignore
-		assert( strpos( $host, 'mysql' ) !== false || strpos( $host, 'mariadb' ) !== false );
-		$wpdb->is_mysql = true;
-		$wpdb->charset  = 'utf8mb4';
-
-		return $wpdb;
-	}
 
 	/**
 	 * 現在のデータベースに存在している、本プラグインで使用するテーブル一覧を取得します。
