2686cfdccc029aef94bb907dbc6789ab114cc4b0
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-07-02 21:22:11 +0900

    関数名修正

--- src/block/provider/postSetting/useSavePostSettingCallback.ts
+++ src/block/provider/postSetting/useSavePostSettingCallback.ts
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { PostSettingContext } from './PostSettingProvider';
 
-export const usePostSetting = () => {
+export const useSavePostSettingCallback = () => {
 	const context = useContext( PostSettingContext );
 	if ( ! context ) {
 		throw new Error( '{A8C089B2-824F-4BA5-8120-7FBFE42F36B2}' );
