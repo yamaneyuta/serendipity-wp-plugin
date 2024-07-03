90203bd122c4290337c2f6cb8c38e04b15b1bbc5
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-07-03 15:14:40 +0900

    販売可能な通貨シンボル一覧を返すResolverを作成
    - 現在はダミーデータを返している

--- includes/classes/Lib/Security/Access.php
+++ includes/classes/Lib/Security/Access.php
@@ -21,4 +21,12 @@ class Access {
 	public function canCurrentUserEditPost( int $post_ID ): bool {
 		return current_user_can( 'edit_post', $post_ID );
 	}
+
+	/**
+	 * 現在アクセスしているユーザーが、投稿を新しく作成できるかどうかを返します。
+	 */
+	public function canCurrentUserCreatePost(): bool {
+		// 寄稿者(contributor)以上の権限がある場合、投稿を作成可能。
+		return current_user_can( 'edit_posts' );
+	}
 }
