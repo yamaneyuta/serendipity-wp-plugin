73c5205e48065e6b6d782cb4eaf1f9ac8bc61f79
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-07-04 10:36:51 +0900

    数量を入力するコンポーネント作成

---
+++ src/block/components/BlockAmountInput.tsx
@@ -0,0 +1,37 @@
+import { useMemo } from 'react';
+import { BlockInput } from './BlockInput';
+
+/**
+ * BlockAmountInputコンポーネントのpropsを定義
+ *
+ * @dev BlockInputのpropsを継承した型を作成。`type`等のプロパティは固定値を設定するため、BlockAmountInputPropsには含めない。
+ */
+export interface BlockAmountInputProps extends Omit< React.InputHTMLAttributes< HTMLInputElement >, 'type' | 'min' > {}
+
+export const TEST_ID = '6f61a5eb';
+
+/**
+ * 数量、小数点以下桁数を元に数値を表示するコンポーネント
+ * @param props
+ */
+export const BlockAmountInput: React.FC< BlockAmountInputProps > = ( props ) => {
+	return <BlockInput data-testid={ TEST_ID } { ...useAmountInputProps( props ) } />;
+};
+
+const useAmountInputProps = ( props: React.ComponentProps< 'input' > ) => {
+	return useMemo(
+		() => ( {
+			...props,
+			type: 'number',
+			min: 0,
+			onKeyDown: ( e: React.KeyboardEvent< HTMLInputElement > ) => {
+				if ( [ '-', '+', 'e' ].includes( e.key ) ) {
+					e.preventDefault();
+				} else {
+					props.onKeyDown?.( e );
+				}
+			},
+		} ),
+		[ props ]
+	);
+};
