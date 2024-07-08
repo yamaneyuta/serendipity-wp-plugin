21dbc844c9f3b9b724604e14bd61a001c4ac9a8d
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-07-08 12:45:14 +0900

    OptionKey.phpにコメント追加

--- includes/classes/Lib/Repository/Option/OptionKey.php
+++ includes/classes/Lib/Repository/Option/OptionKey.php
@@ -6,8 +6,11 @@ namespace Cornix\Serendipity\Core\Lib\Repository\Option;
 use Cornix\Serendipity\Core\Lib\SystemInfo\PluginInfo;
 
 final class OptionKey {
+
+	/** データベーススキーマバージョンを保存するオプションキー */
 	public const DB_SCHEMA_VERSION = 'db_schema_version';
 
+	/** プラグインの設定を保存するオプションキーのプレフィックスを取得します。 */
 	public static function prefix(): string {
 		$prefix = ( new PluginInfo() )->optionNamePrefix();
 		assert( ! empty( $prefix ) );
