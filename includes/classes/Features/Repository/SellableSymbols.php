13d6b3e216604281f5ed030fa75887ae95eabf02
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-07-08 12:50:56 +0900

    暫定でメインネットで販売可能な通貨シンボル一覧を返す実装

--- includes/classes/Features/Repository/SellableSymbols.php
+++ includes/classes/Features/Repository/SellableSymbols.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Features\Repository;
 
+use Cornix\Serendipity\Core\Lib\Repository\OracleData;
+
 class SellableSymbols {
 
 	/**
@@ -11,7 +13,14 @@ class SellableSymbols {
 	 * @return string[]
 	 */
 	public function get(): array {
-		// TODO: 販売価格として設定可能な通貨シンボル一覧を返す
-		return array( 'USD', 'JPY', 'ETH' );  // ダミーデータ
+		error_log( '{DF2EDA7F-484D-4631-8AD3-924A6BAB1D79}' );
+		$chain_id = 1;  // TODO: 販売しようとしているネットワークに対するチェーンIDを取得する
+
+		// レート変換可能な通貨シンボル一覧を取得
+		$oracle_symbols = ( new OracleData() )->getSymbols( $chain_id );
+		// USDを追加
+		$oracle_symbols[] = 'USD';
+
+		return $oracle_symbols;
 	}
 }
