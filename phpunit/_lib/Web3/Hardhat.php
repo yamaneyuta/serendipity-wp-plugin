855eb63e37f6a2dae7196e3ebea9cd53c6685e22
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-07-13 20:06:14 +0900

    Hardhatへのアクセス用クラスを作成
    - 自動マイニング設定を取得する関数を仮で実装

---
+++ phpunit/_lib/Web3/Hardhat.php
@@ -0,0 +1,84 @@
+<?php
+declare(strict_types=1);
+
+use Cornix\Serendipity\Core\Lib\Strings\Strings;
+use Web3\Methods\EthMethod;
+use Web3\Providers\HttpProvider;
+use Web3\Providers\Provider;
+
+class Hardhat {
+	public function __construct( string $rpc_url ) {
+		$this->rpc_url = $rpc_url;
+	}
+	private string $rpc_url;
+
+	/**
+	 * 自動マイニングが有効かどうかを取得します。
+	 */
+	public function getAutomine() {
+		$provider = new HardhatProvider( $this->rpc_url );
+
+		$is_auto_mine = false;
+		$provider->getAutomine(
+			function ( $err, $res ) use ( &$is_auto_mine ) {
+				if ( $err ) {
+					throw $err;
+				}
+				assert( is_bool( $res ), '[24270968] The result must be boolean.' );
+				$is_auto_mine = $res;
+			}
+		);
+
+		return $is_auto_mine;
+	}
+}
+
+
+/**
+ * @internal
+ */
+class HardhatProvider {
+	public function __construct( string $rpc_url, $timeout = 1 ) {
+		assert( Strings::starts_with( $rpc_url, 'http://' ) ); // hardhatはhttpプロトコルのみ対応
+		$this->provider = new HttpProvider( $rpc_url, $timeout );
+	}
+
+	private Provider $provider;
+
+
+	public function __call( $name, $arguments ) {
+		$callback = array_pop( $arguments );
+		if ( ! is_callable( $callback ) ) {
+			throw new \InvalidArgumentException( '[485A40CB] The last param must be callback function.' );
+		}
+
+		$methodObject = $this->getMethodObject( $name, $arguments );
+		if ( $methodObject->validate( $arguments ) ) {
+			$inputs                  = $methodObject->transform( $arguments, $methodObject->inputFormatters );
+			$methodObject->arguments = $inputs;
+			/** @disregard P1013 Undefined method */
+			return $this->provider->send( $methodObject, $callback );
+		}
+	}
+
+
+	private function getMethodObject( string $method, array $arguments ) {
+		switch ( $method ) {
+			case 'getAutomine':
+				return new GetAutomine( 'hardhat_getAutomine', $arguments );
+			default:
+				throw new \InvalidArgumentException( '[BA4B5347] Invalid method. - method: ' . $method );
+		}
+	}
+}
+
+
+/**
+ * @internal
+ */
+class GetAutomine extends EthMethod {
+	protected $validators       = array();
+	protected $inputFormatters  = array();
+	protected $outputFormatters = array();
+	protected $defaultValues    = array();
+}
