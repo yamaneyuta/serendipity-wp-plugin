1b2ddb20772feb787f15fa404a60e2828d787c8a
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-07-13 17:58:15 +0900

    *.dev.local -> .local

--- phpunit/_lib/Web3/TestRPCUrl.php
+++ phpunit/_lib/Web3/TestRPCUrl.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 class TestRPCUrl {
 	public function privatenetL1(): string {
 		// メインネット相当のテスト用RPC URL
-		return 'http://privatenet-1.tests.local';   // (compose.ymlに記載のhostname)
+		return 'http://tests-privatenet-1.local';   // (compose.ymlに記載のhostname)
 	}
 
 	public function privatenetL2(): string {
 		// Polygonネットワーク相当のテスト用RPC URL
-		return 'http://privatenet-2.tests.local';   // (compose.ymlに記載のhostname)
+		return 'http://tests-privatenet-2.local';   // (compose.ymlに記載のhostname)
 	}
 }
