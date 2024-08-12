1b809bce305a3a89d96a15099e14c4185ec921c7
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-12 19:44:10 +0900

    開発用チェーンのリポジトリからpullを正常に完了できない可能性があるので最新のバージョンにリセットする方式に変更

--- .devcontainer/development-internal/privatenet/entrypoint.sh
+++ .devcontainer/development-internal/privatenet/entrypoint.sh
@@ -3,11 +3,15 @@
 # `serendipity-dev-blockchain`ディレクトリが存在しない場合はclone
 # 存在する場合は最新の状態に更新
 if [ ! -d serendipity-dev-blockchain ]; then
+  echo "clone repository"
   git clone https://github.com/yamaneyuta/serendipity-dev-blockchain
   cd serendipity-dev-blockchain
 else
+	echo "update repository"
   cd serendipity-dev-blockchain
-  git pull
+  # git pull
+	git fetch origin main
+	git reset --hard origin/main
 fi
 
 npm install
