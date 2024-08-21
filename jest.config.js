935a7048c7ee22b8f92f45b6d028b0520a6ce3e0
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-21 19:09:38 +0900

    jest.config.jsの`testEnvironment`を`jsdom`に変更

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
   },
