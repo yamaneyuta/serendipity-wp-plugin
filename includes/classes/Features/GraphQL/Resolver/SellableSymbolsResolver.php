11be3a099c310e3184f84453004e901544dcb42a
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-21 14:39:59 +0900

    SellableSymbolsResolverの戻り値を修正
    - プライベートネットの販売可能通貨シンボル一覧を取得する際、プライベートネットに接続できない場合はnullを返すように変更

--- includes/classes/Features/GraphQL/Resolver/SellableSymbolsResolver.php
+++ includes/classes/Features/GraphQL/Resolver/SellableSymbolsResolver.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace Cornix\Serendipity\Core\Features\GraphQL\Resolver;
 
 use Cornix\Serendipity\Core\Features\Repository\SellableSymbols;
+use Cornix\Serendipity\Core\Lib\Enum\NetworkType;
 use Cornix\Serendipity\Core\Lib\Security\Access;
+use Cornix\Serendipity\Core\Lib\Web3\Blockchain;
 
 /**
  * 販売価格として設定可能な通貨シンボル一覧を返すリゾルバです。
@@ -24,6 +26,11 @@ class SellableSymbolsResolver extends ResolverBase {
 
 		/** @var string */
 		$network_type = $args['networkType'];
+
+		// プライベートネットが指定され、プライベートネットに接続できない場合はnullを返す。
+		if ( $network_type === NetworkType::PRIVATENET && ! Blockchain::isPrivatenetConnectable() ) {
+			return null;
+		}
 
 		return ( new SellableSymbols() )->get( $network_type );
 	}
