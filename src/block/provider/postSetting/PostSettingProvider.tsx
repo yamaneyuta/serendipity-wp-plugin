45ed045377dc69cc012dcad6a9ff3f916ccf6f77
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-22 11:32:02 +0900

    受信したデータの整合性チェックの場所を移動

--- src/block/provider/postSetting/PostSettingProvider.tsx
+++ src/block/provider/postSetting/PostSettingProvider.tsx
@@ -1,5 +1,10 @@
 import { createContext, useCallback } from 'react';
-import { PostSettingInput, usePostSettingQuery, useSetPostSettingMutation } from '../../../types/gql/generated';
+import {
+	PostSettingInput,
+	PostSettingQuery,
+	usePostSettingQuery,
+	useSetPostSettingMutation,
+} from '../../../types/gql/generated';
 import { usePostID } from '../postID/usePostID';
 
 type PostSettingType = ReturnType< typeof _usePostSetting >;
@@ -9,6 +14,7 @@ export const PostSettingContext = createContext< PostSettingType | undefined >( 
 const _usePostSetting = () => {
 	const postID = usePostID();
 	const { data, refetch } = usePostSettingQuery( { postID } );
+	checkPostSetting( data ); // データの整合性チェック
 	const { mutateAsync } = useSetPostSettingMutation( {
 		onSuccess: async () => {
 			await refetch();
@@ -33,6 +39,24 @@ const _usePostSetting = () => {
 
 type PostSettingProviderProps = {
 	children: React.ReactNode;
+};
+
+/**
+ * サーバーから受信したデータの整合性をチェックし、問題があればエラーをスローします。
+ * @param postSetting
+ */
+const checkPostSetting = ( postSetting: PostSettingQuery | undefined ) => {
+	if ( postSetting ) {
+		if ( ! postSetting.mainnetSellableSymbols === undefined ) {
+			throw new Error( '[1ED2539F] mainnetSellableSymbols is not defined' );
+		}
+		if ( ! postSetting.testnetSellableSymbols === undefined ) {
+			throw new Error( '[7F7A9241] testnetSellableSymbols is not defined' );
+		}
+		if ( postSetting.privatenetSellableSymbols === undefined ) {
+			throw new Error( '[B1F3CD99] privatenetSellableSymbols is not defined' );
+		}
+	}
 };
 
 export const PostSettingProvider: React.FC< PostSettingProviderProps > = ( { children } ) => {
