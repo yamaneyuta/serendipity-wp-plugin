22d08520b37cb5290be8b2fce756f85c3ae1a7f8
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-22 11:32:48 +0900

    useSelectableSymbols.test.tsxでカバー可能なテストを削除

--- src/block/features/selectValues/useGetSellableSymbolsCallback.test.tsx
+++ src/block/features/selectValues/useGetSellableSymbolsCallback.test.tsx
@@ -1,3 +1,4 @@
+import assert from 'node:assert/strict';
 import { NetworkType } from '../../../types/gql/generated';
 import { useGetSellableSymbolsCallback as sut } from './useGetSellableSymbolsCallback';
 import { usePostSetting } from '../../provider/postSetting/usePostSetting';
@@ -7,88 +8,15 @@ jest.mock( '../../provider/postSetting/usePostSetting' );
 
 type UsePostSettingResult = ReturnType< typeof usePostSetting >;
 
-/**
- * 通常のテスト。各ネットワークで販売可能な通貨シンボルを取得できる場合。
+/*
+ * 基本的なテストは./useSelectableSymbols.test.tsxで実施。
+ * ここでは、カバーしていないケースを中心にテストする。
  */
-it( 'useGetSellableSymbolsCallback() - default', async () => {
-	// ARRANGE
-	const res: UsePostSettingResult = {
-		mainnetSellableSymbols: [ 'JPY' ],
-		testnetSellableSymbols: [ 'USD' ],
-		privatenetSellableSymbols: [ 'EUR', 'GBP' ],
-		sellingNetwork: null,
-		sellingPrice: null,
-	};
-	( usePostSetting as jest.Mock ).mockReturnValue( res );
-	const { result: getMainnet } = renderHook( () => sut( NetworkType.Mainnet ) );
-	const { result: getTestnet } = renderHook( () => sut( NetworkType.Testnet ) );
-	const { result: getPrivatenet } = renderHook( () => sut( NetworkType.Privatenet ) );
-
-	// ACT
-	const mainnetSellableSymbols = getMainnet.current();
-	const testnetSellableSymbols = getTestnet.current();
-	const privatenetSellableSymbols = getPrivatenet.current();
-
-	// ASSERT
-	expect( mainnetSellableSymbols ).toEqual( [ 'JPY' ] );
-	expect( testnetSellableSymbols ).toEqual( [ 'USD' ] );
-	expect( privatenetSellableSymbols ).toEqual( [ 'EUR', 'GBP' ] );
-} );
-
-/**
- * 投稿設定をサーバーから取得している最中のテスト。
- */
-it( 'useGetSellableSymbolsCallback() - loading', async () => {
-	// ARRANGE
-	const res: UsePostSettingResult = undefined;
-	( usePostSetting as jest.Mock ).mockReturnValue( res );
-	const { result: getMainnet } = renderHook( () => sut( NetworkType.Mainnet ) );
-	const { result: getTestnet } = renderHook( () => sut( NetworkType.Testnet ) );
-	const { result: getPrivatenet } = renderHook( () => sut( NetworkType.Privatenet ) );
-
-	// ACT
-	const mainnetSellableSymbols = getMainnet.current();
-	const testnetSellableSymbols = getTestnet.current();
-	const privatenetSellableSymbols = getPrivatenet.current();
-
-	// ASSERT
-	expect( mainnetSellableSymbols ).toBeUndefined();
-	expect( testnetSellableSymbols ).toBeUndefined();
-	expect( privatenetSellableSymbols ).toBeUndefined();
-} );
-
-/**
- * 販売可能な通貨シンボルが取得できなかった場合のテスト。
- */
-it( 'useGetSellableSymbolsCallback() - null', async () => {
-	// ARRANGE
-	const res: UsePostSettingResult = {
-		mainnetSellableSymbols: null,
-		testnetSellableSymbols: null,
-		privatenetSellableSymbols: null,
-		sellingNetwork: null,
-		sellingPrice: null,
-	};
-	( usePostSetting as jest.Mock ).mockReturnValue( res );
-	const { result: getMainnet } = renderHook( () => sut( NetworkType.Mainnet ) );
-	const { result: getTestnet } = renderHook( () => sut( NetworkType.Testnet ) );
-	const { result: getPrivatenet } = renderHook( () => sut( NetworkType.Privatenet ) );
-
-	// ACT
-	const mainnetSellableSymbols = getMainnet.current();
-	const testnetSellableSymbols = getTestnet.current();
-	const privatenetSellableSymbols = getPrivatenet.current();
-
-	// ASSERT
-	expect( mainnetSellableSymbols ).toBeNull();
-	expect( testnetSellableSymbols ).toBeNull();
-	expect( privatenetSellableSymbols ).toBeNull();
-} );
 
 /**
  * 無効なネットワーク種別が指定された場合のテスト。
  */
-it( 'useGetSellableSymbolsCallback() - null', async () => {
+it( 'useGetSellableSymbolsCallback() - invalid network', async () => {
 	// ARRANGE
 	const res: UsePostSettingResult = {
 		mainnetSellableSymbols: [ 'JPY' ],
@@ -106,6 +34,34 @@ it( 'useGetSellableSymbolsCallback() - null', async () => {
 		expect( true ).toBeFalsy(); // ここには到達しない
 	} catch ( e ) {
 		// ASSERT
-		expect( e instanceof Error ).toBeTruthy();
+		assert( e instanceof Error );
+		expect( e.message ).toContain( '[3D102039]' );
+	}
+} );
+
+/**
+ * APIの戻り値が不正な場合のテスト。
+ */
+it( 'useGetSellableSymbolsCallback() - invalid response', async () => {
+	// ARRANGE
+	const res: UsePostSettingResult = {
+		mainnetSellableSymbols: undefined, // 不正な値(APIの仕様上、undefinedになることはない)
+		testnetSellableSymbols: [ 'USD' ],
+		privatenetSellableSymbols: [ 'EUR', 'GBP' ],
+		sellingNetwork: null,
+		sellingPrice: null,
+	};
+	( usePostSetting as jest.Mock ).mockReturnValue( res );
+
+	const { result: getMainnet } = renderHook( () => sut( NetworkType.Mainnet ) );
+
+	try {
+		// ACT
+		getMainnet.current();
+		expect( true ).toBeFalsy(); // ここには到達しない
+	} catch ( e ) {
+		// ASSERT
+		assert( e instanceof Error );
+		expect( e.message ).toContain( '[519DA805]' );
 	}
 } );
