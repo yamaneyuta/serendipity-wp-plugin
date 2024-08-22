1fa66ba45c6883013a07f8a99c24325cdbfec0d6
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-22 13:43:45 +0900

    data-testidの付与をstoriesファイルへ移動

--- src/block/components/BlockAmountInput.tsx
+++ src/block/components/BlockAmountInput.tsx
@@ -1,21 +1,22 @@
 import { useMemo } from 'react';
 import { BlockInput } from './BlockInput';
+import { IDataTestIdProps } from './IDataTestIdProps';
 
 /**
  * BlockAmountInputコンポーネントのpropsを定義
  *
  * BlockInputのpropsを継承した型を作成。`type`等のプロパティは固定値を設定するため、BlockAmountInputPropsには含めない。
  */
-export interface BlockAmountInputProps extends Omit< React.InputHTMLAttributes< HTMLInputElement >, 'type' | 'min' > {}
-
-export const TEST_ID = '6f61a5eb';
+export interface BlockAmountInputProps
+	extends IDataTestIdProps,
+		Omit< React.InputHTMLAttributes< HTMLInputElement >, 'type' | 'min' > {}
 
 /**
  * 数量、小数点以下桁数を元に数値を表示するコンポーネント
  * @param props
  */
 export const BlockAmountInput: React.FC< BlockAmountInputProps > = ( props ) => {
-	return <BlockInput data-testid={ TEST_ID } { ...useAmountInputProps( props ) } />;
+	return <BlockInput { ...useAmountInputProps( props ) } />;
 };
 
 const useAmountInputProps = ( props: React.ComponentProps< 'input' > ) => {
---
+++ src/block/components/IDataTestIdProps.ts
@@ -0,0 +1,3 @@
+export interface IDataTestIdProps {
+	'data-testid'?: string;
+}
