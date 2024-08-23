5b688102412f0ca818328b64cc44db773504fad9
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-23 14:33:51 +0900

    販売ネットワークがユーザーによって変更されたかどうかを取得するhooksを追加

---
+++ src/block/features/watch/dataChanged/useIsSelectedNetworkChanged.ts
@@ -0,0 +1,19 @@
+import { useSellingNetwork } from '../../../provider/serverData/useSellingNetwork';
+import { useSelectedNetwork } from '../../../provider/userInput/selectedNetwork/useSelectedNetwork';
+
+/**
+ * 販売するネットワークが画面上で変更されたかどうかを取得します。
+ */
+export const useIsSelectedNetworkChanged = () => {
+	// サーバーから取得した販売ネットワーク
+	const sellingNetwork = useSellingNetwork();
+	// ユーザーが選択したネットワーク
+	const selectedNetwork = useSelectedNetwork().selectedNetwork;
+
+	// 読み込み中はデータが変更されたとみなさない
+	if ( sellingNetwork === undefined || selectedNetwork === undefined ) {
+		return false;
+	}
+
+	return sellingNetwork !== selectedNetwork;
+};
