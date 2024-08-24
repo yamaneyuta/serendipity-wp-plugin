b733073f66e73a7fe451c58e754d4638c78afa85
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-24 14:38:29 +0900

    useAutoSavePostSettingリファクタ

--- src/block/features/watch/useAutoSavePostSetting.ts
+++ src/block/features/watch/useAutoSavePostSetting.ts
@@ -1,44 +1,69 @@
 import { useEffect, useMemo } from 'react';
+import AsyncLock from 'async-lock';
+import { inputValueToAmount } from '@yamaneyuta/serendipity-lib-js-price-format';
 import { PostSettingInput } from '../../../types/gql/generated';
 import { useEditorProperty } from '../../provider/editor/useEditorProperty';
 import { useSavePostSettingCallback } from '../../provider/serverData/postSetting/useSavePostSettingCallback';
-import { ScreenPostSetting } from '../screenData/ScreenPostSetting.type';
 import { useIsDataChanged } from './useIsDataChanged';
+import { useInputPriceValue } from '../../provider/userInput/inputPriceValue/useInputPriceValue';
+import { useSelectedNetwork } from '../../provider/userInput/selectedNetwork/useSelectedNetwork';
+import { useSelectedPriceSymbol } from '../../provider/userInput/selectedPriceSymbol/useSelectedPriceSymbol';
 
 /**
  * 投稿編集画面で、投稿が手動で保存された時に設定も保存します。
- * @param postSetting
  */
-export const useAutoSavePostSetting = ( postSetting: ScreenPostSetting ) => {
+export const useAutoSavePostSetting = () => {
+	const lock = useMemo( () => new AsyncLock(), [] );
 	const isManualSaving = useIsManualSaving();
-	const save = useSavePostSettingCallback();
 	const isDataChanged = useIsDataChanged();
+	const save = useSavePostSettingCallback();
+	const postSettingInput = usePostSettingInput();
 
 	useEffect( () => {
 		if ( ! isManualSaving || ! isDataChanged ) {
 			return;
 		}
 
-		save( convertToPostSettingInput( postSetting ) );
-	}, [ postSetting, isManualSaving, save, isDataChanged ] );
+		if ( ! postSettingInput ) {
+			throw new Error( '[136AA840] Invalid postSettingInput' );
+		}
+
+		if ( ! lock.isBusy() ) {
+			lock.acquire( '{77F905B2-764C-409C-B8D9-F3D757D8A790}', async () => {
+				await save( postSettingInput );
+			} );
+		}
+	}, [ lock, isManualSaving, isDataChanged, save, postSettingInput ] );
 };
 
-const convertToPostSettingInput = ( postSetting: ScreenPostSetting ): PostSettingInput => {
-	const sellingPrice = postSetting.sellingPrice;
-	if ( sellingPrice === undefined || ! sellingPrice?.symbol ) {
-		throw new Error( '[416A72D9] sellingPrice: ' + JSON.stringify( sellingPrice ) );
+/**
+ * サーバーへ登録するための値を取得します。
+ */
+const usePostSettingInput = (): PostSettingInput | null | undefined => {
+	// ユーザーが選択したネットワーク種別
+	const { selectedNetwork } = useSelectedNetwork();
+	// ユーザーが入力した価格の値
+	const { inputPriceValue } = useInputPriceValue();
+	// ユーザーが選択した価格の通貨シンボル
+	const { selectedPriceSymbol } = useSelectedPriceSymbol();
+
+	// 読み込み中の値がある場合はundefinedを返す
+	if ( selectedNetwork === undefined || inputPriceValue === undefined || selectedPriceSymbol === undefined ) {
+		return undefined;
 	}
-	if ( ! postSetting.sellingNetwork ) {
-		throw new Error( '[77E7B5AE] postSetting.sellingNetwork: ' + JSON.stringify( postSetting.sellingNetwork ) );
+	// いずれかの値がnullである場合はnullを返す(サーバー側で登録できないためあらかじめフロントエンドでnullとして扱う)
+	if ( selectedNetwork === null || inputPriceValue === null || selectedPriceSymbol === null ) {
+		return null;
 	}
 
+	const { amount, decimals } = inputValueToAmount( inputPriceValue );
 	return {
+		sellingNetwork: selectedNetwork,
 		sellingPrice: {
-			amountHex: sellingPrice.amountHex,
-			decimals: sellingPrice.decimals,
-			symbol: sellingPrice.symbol,
+			amountHex: '0x' + amount.toString( 16 ),
+			decimals,
+			symbol: selectedPriceSymbol,
 		},
-		sellingNetwork: postSetting.sellingNetwork,
 	};
 };
 
