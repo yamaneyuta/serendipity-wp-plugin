fdeb28e4394f7207da36a8d82d8c9d13d27fb45a
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-08-25 11:31:28 +0900

    jest.config.js修正
    - lib-frontendのための設定を削除

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+	preset: 'ts-jest',
 	testEnvironment: "jsdom",	// Reactのテストをする場合はjsdomを指定
-	transform: {
-		// `lib-frontend`のライブラリ読み込み時のエラーを解消するために
-		// `"allowJs": true`を指定した`tsconfig.jest.json`を使用
-	    "^.+\\.m?[tj]sx?$": ['ts-jest', { tsconfig: "tsconfig.jest.json" }],
-	},
-	// `lib-frontend`ライブラリがトランスパイルを必要とするが、`node_modules`にあるため、設定を追加
-	transformIgnorePatterns: ["node_modules/(?!.*(lib-frontend))"],
-
 	coverageDirectory: "coverage/jest",
 };
