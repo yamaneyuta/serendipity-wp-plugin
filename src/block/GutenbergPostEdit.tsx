1c14e0b4c75b3e624a452524605350f90f6fc075
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-03 11:34:41 +0000

    delete: useNotifyDataChangedToEditor

--- src/block/GutenbergPostEdit.tsx
+++ src/block/GutenbergPostEdit.tsx
@@ -5,7 +5,6 @@ import { SymbolSelect } from './features/symbolSelect/SymbolSelect';
 import { NetworkSelect } from './features/networkSelect/NetworkSelect';
 import { useAutoBindServerData } from './features/watch/useAutoBindServerData';
 import { useNetworkSelectProps } from './features/networkSelect/useNetworkSelectProps';
-import { useNotifyDataChangedToEditor } from './features/watch/useNotifyDataChangedToEditor';
 import { useSymbolSelectProps } from './features/symbolSelect/useSymbolSelectProps';
 import { usePriceValueInputProps } from './features/priceValueInput/usePriceValueInputProps';
 import { PriceValueInput } from './features/priceValueInput/PriceValueInput';
@@ -17,8 +16,6 @@ type GutenbergPostEditProps = {
 export const GutenbergPostEdit: React.FC< GutenbergPostEditProps > = ( { onDataChanged } ) => {
 	// サーバーから取得した情報を画面に反映
 	useAutoBindServerData();
-	// データが変更された時にWordPressのエディタに通知
-	useNotifyDataChangedToEditor( onDataChanged );
 	// 投稿保存時に設定情報を保存
 	useAutoSavePostSetting();
 
