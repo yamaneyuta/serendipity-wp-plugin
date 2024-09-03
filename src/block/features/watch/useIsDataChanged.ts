41cf9bdab2898f5ab4c868cce453123a051d1606
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-03 11:50:37 +0000

    delete: useIsSellingPriceSymbolChanged, useIsDataChanged

--- src/block/features/watch/useIsDataChanged.ts
+++
@@ -1,8 +0,0 @@
-import { useIsSellingPriceSymbolChanged } from './dataChanged/useIsSellingPriceSymbolChanged';
-
-export const useIsDataChanged = () => {
-	// 販売価格の通貨シンボルが変更されたかどうか
-	const isSellingPriceSymbolChanged = useIsSellingPriceSymbolChanged();
-
-	return isSellingPriceSymbolChanged;
-};
