322b4e5c5419becef09c1f0aa3915464c6928cc8
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-04 02:49:14 +0000

    refactor: useSelectableSymbolsの引数を削除

--- src/block/features/symbolSelect/useSelectableSymbols.ts
+++ src/block/features/symbolSelect/useSelectableSymbols.ts
@@ -1,25 +1,27 @@
 import { useMemo } from 'react';
-import { NetworkType } from '../../../types/gql/generated';
 import { useGetSellableSymbolsCallback } from '../../provider/serverData/useGetSellableSymbolsCallback';
+import { useSelectedNetwork } from '../../provider/widgetState/selectedNetwork/useSelectedNetwork';
 
 /**
  * 画面で選択可能な通貨シンボル一覧を取得します。
- * @param sellingNetwork
  */
-export const useSelectableSymbols = ( sellingNetwork: NetworkType | null | undefined ): string[] | null | undefined => {
+export const useSelectableSymbols = (): string[] | null | undefined => {
+	// 画面で選択されているネットワーク
+	const { selectedNetwork } = useSelectedNetwork();
+	// ネットワークに応じた販売可能な通貨シンボル一覧を取得するコールバック
 	const getSellableSymbols = useGetSellableSymbolsCallback();
 
 	return useMemo( () => {
-		if ( sellingNetwork === undefined ) {
+		if ( selectedNetwork === undefined ) {
 			// 画面初期化中の場合
 			return undefined;
 		}
-		if ( sellingNetwork === null ) {
+		if ( selectedNetwork === null ) {
 			// 販売ネットワークが未指定の場合
 			return null;
 		}
 		// 指定されたネットワークで販売可能な通貨シンボル一覧を取得
 		// (販売可能な通貨シンボル一覧をAPIから取得できていない状態の場合はundefinedが返る)
-		return getSellableSymbols( sellingNetwork );
-	}, [ sellingNetwork, getSellableSymbols ] );
+		return getSellableSymbols( selectedNetwork );
+	}, [ selectedNetwork, getSellableSymbols ] );
 };
