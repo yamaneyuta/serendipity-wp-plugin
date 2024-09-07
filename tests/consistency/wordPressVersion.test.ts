dc69bfb5a1a667e8b89b3e0eb2b44e1ee6613866
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-07 09:00:06 +0000

    add: readme.txtに記載の`Requires at least`をチェックするテストを追加

--- tests/consistency/wordPressVersion.test.ts
+++ tests/consistency/wordPressVersion.test.ts
@@ -2,6 +2,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import { load } from 'js-yaml';
 import { EntryPhpFile } from '../lib/EntryPhpFile';
+import { ReadmeTxt } from '../lib/ReadmeTxt';
 
 /**
  * プラグインバージョンの整合性チェック
@@ -19,4 +20,18 @@ it( '[975682DD] WordPress Requires at latest version is tested', async () => {
 
 	// 取得したテスト済みWordPressのバージョンの一番古いものが、PHPに記載されている最低限必要なバージョンと一致していることを確認
 	expect( oldestVersion ).toBe( requiresAtLeast );
+} );
+
+/**
+ * PHPに記載の最低限必要なWordPressバージョンとreadme.txtに記載の最低限必要なWordPressバージョンが一致していることを確認する。
+ */
+it( '[0491C5A3] WordPress Requires at latest version is tested', async () => {
+	// エントリファイルとなるPHPから最低限必要なWordPressバージョンを取得
+	const phpRequiresAtLeast = EntryPhpFile.getRequiresAtLeast();
+
+	// readme.txtから最低限必要なWordPressバージョンを取得
+	const readmeRequiresAtLeast = ReadmeTxt.getRequiresAtLeast();
+
+	// 両方のバージョンが一致していることを確認
+	expect( readmeRequiresAtLeast ).toBe( phpRequiresAtLeast );
 } );
