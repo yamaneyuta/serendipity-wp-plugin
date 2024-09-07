5e1fbfb526de00ab36c60f7ca2a37987b98d53e6
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-07 09:10:00 +0000

    add: テスト済みWordPressバージョンがreadme.txtに反映されていることを確認するテスト追加

--- tests/consistency/wordPressVersion.test.ts
+++ tests/consistency/wordPressVersion.test.ts
@@ -23,6 +23,23 @@ it( '[975682DD] WordPress Requires at latest version is tested', async () => {
 } );
 
 /**
+ * readme.txtに記載の`Tested up to`のバージョンがCIの設定に含まれていることを確認する。
+ */
+it( '[F4DD3D0A] WordPress Tested up to version is tested', async () => {
+	// readme.txtから最新テスト済みWordPressバージョンを取得
+	const testedUpTo = ReadmeTxt.getTestedUpTo();
+
+	// GitHub Actionsの設定ファイルからテストを実行しているWordPressのバージョン一覧を取得
+	const workflow = load( fs.readFileSync( path.resolve( process.cwd(), '.github/workflows/ci.yml' ), 'utf-8' ) );
+	const matrix: { 'wordpress-version': string }[] = ( workflow as any ).jobs.ci.strategy.matrix.env;
+	const versions = matrix.map( ( v ) => v[ 'wordpress-version' ] );
+	const latestTestedVersion = versions[ versions.length - 1 ];
+
+	// 取得したWordPressのバージョン一覧に、readme.txtに記載されている最新テスト済みバージョンが含まれていることを確認
+	expect( latestTestedVersion ).toBe( testedUpTo );
+} );
+
+/**
  * PHPに記載の最低限必要なWordPressバージョンとreadme.txtに記載の最低限必要なWordPressバージョンが一致していることを確認する。
  */
 it( '[0491C5A3] WordPress Requires at latest version is tested', async () => {
