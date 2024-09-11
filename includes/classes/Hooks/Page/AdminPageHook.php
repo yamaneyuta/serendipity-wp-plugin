a8f17fabc82f038a886bd193a0812b0ea6c2fa80
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-11 04:37:22 +0000

    add: 管理画面でjavascriptファイルを読み込めるように処理を追加

--- includes/classes/Hooks/Page/AdminPageHook.php
+++ includes/classes/Hooks/Page/AdminPageHook.php
@@ -2,6 +2,9 @@
 declare(strict_types=1);
 namespace Cornix\Serendipity\Core\Hooks\Page;
 
+use Cornix\Serendipity\Core\Features\ExportToJS\RestVer;
+use Cornix\Serendipity\Core\Lib\Path\ProjectFile;
+use Cornix\Serendipity\Core\Lib\Repository\HandleName;
 use Cornix\Serendipity\Core\Lib\Repository\I18n;
 use Cornix\Serendipity\Core\Lib\Repository\Slug;
 
@@ -10,6 +13,9 @@ class AdminPageHook {
 	public function register(): void {
 		// 管理画面のメニュー追加。
 		add_action( 'admin_menu', array( $this, 'addActionAdminMenu' ) );
+
+		// 管理画面のスクリプト読み込み。
+		add_action( 'admin_enqueue_scripts', array( $this, 'addActionAdminEnqueueScripts' ) );
 	}
 
 	public function addActionAdminMenu(): void {
@@ -22,8 +28,8 @@ class AdminPageHook {
 		$slug = new Slug();
 		$i18n = new I18n();
 
-		$capability = 'manage_options'; // ユーザー権限(`manage_options`は、管理画面の`設定`へアクセス可能な権限)
-		$page_callback = function() {
+		$capability    = 'manage_options'; // ユーザー権限(`manage_options`は、管理画面の`設定`へアクセス可能な権限)
+		$page_callback = function () {
 			$div_id = 'b1196eb9-a07c-4c41-9d75-ee6830ce7321';
 			echo '<div id="' . esc_attr( $div_id ) . '"></div>';
 		};
@@ -53,5 +59,31 @@ class AdminPageHook {
 
 		// デフォルトで追加される、トップレベルメニューと同じ表示名のサブメニューを削除
 		remove_submenu_page( $slug->adminMenuRoot(), $slug->adminMenuRoot() );
+	}
+
+	/**
+	 * 管理画面で使用するスクリプトを読み込みます
+	 */
+	public function addActionAdminEnqueueScripts(): void {
+		assert( is_admin() );
+
+		// 管理画面用のスクリプトを登録する際のハンドル名を取得
+		$handle_name = ( new HandleName() )->adminScript();
+
+		// アセットファイルを読み込む
+		$asset_file_path = ( new ProjectFile( 'public/admin/index.asset.php' ) )->toLocalPath();
+		$asset_file      = include $asset_file_path;
+
+		// 管理画面のスクリプト読み込み
+		wp_enqueue_script(
+			$handle_name,
+			( new ProjectFile( 'public/admin/index.js' ) )->toUrl(),
+			$asset_file['dependencies'],
+			$asset_file['version'],
+			true,   // フッターに出力。
+		);
+
+		// インラインスクリプトを追加
+		( new RestVer() )->exportToJS( $handle_name );
 	}
 }
