e018041326ad5aa18bfd5875e3e648256335c215
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-20 05:00:03 +0000

    change: ブロック用のgraphqlクエリが記述されたファイルの検索パスを変更し、高速化

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
 	schema: './includes/assets/graphql/schema.graphql',
-	documents: [ '**/block/*graphql' ],
+	documents: [ './includes/assets/graphql/block/*.graphql' ],
 	ignoreNoDocuments: true, // for better experience with the watcher
 	generates: {
 		'./src/types/gql/generated.ts': { // `preset: 'client'`を使用しない場合はファイル名を指定
