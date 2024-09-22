94a57678672f39e9b5d36cf86b235051467f116f
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-22 11:57:27 +0000

    docs: @tanstack/react-query@5を導入していない理由をcodegen.tsに記載

--- codegen.ts
+++ codegen.ts
@@ -17,6 +17,18 @@ const config: CodegenConfig = {
 				fetcher: '@yamaneyuta/serendipity-lib-frontend#fetcher', // 相対パスの場合は、生成されるファイルからのパス
 				// isReactHook: true,
 				// exposeQueryKeys: true,
+
+				/*
+				// @tanstack/react-query@5に対応する場合は、以下の設定を有効にする
+				// ※ @tanstack/react-query@5.56.2 ではReact18以上("^18 || ^19")が必要
+				//    対して、WordPress組み込みのReact@18になるのは WordPress@6.2以降
+				//    https://make.wordpress.org/core/2023/03/07/upgrading-to-react-18-and-common-pitfalls-of-concurrent-mode/
+				//    WordPress@5.4(2024/9/22時点)で開発しているため、@tanstack/react-query@5は導入していない
+
+				// https://github.com/dotansimha/graphql-code-generator/issues/9786#issuecomment-1938602063
+				addSuspeneQuery: true,
+				reactQueryVersion: 5,
+				*/
 			},
 		}
 	},
