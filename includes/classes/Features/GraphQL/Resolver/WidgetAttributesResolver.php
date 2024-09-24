fadbc5e0afca621c8c2dc66c17701648bdea42d9
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-24 00:28:12 +0000

    fix: 投稿の属性取得時の権限チェックを追加

--- includes/classes/Features/GraphQL/Resolver/WidgetAttributesResolver.php
+++ includes/classes/Features/GraphQL/Resolver/WidgetAttributesResolver.php
@@ -17,6 +17,11 @@ class WidgetAttributesResolver extends ResolverBase {
 		/** @var int */
 		$post_ID = $args['postID'];
 
+		// 投稿が公開済み、または編集可能な権限がある時に設定されている価格を返します。
+		if ( ! $this->isPublishedOrEditable( $post_ID ) ) {
+			throw new \LogicException( '[719FA721] You do not have permission to access this post.' );
+		}
+
 		// 投稿設定を取得します。
 		return ( new WidgetAttributes( new PostContent( $post_ID ) ) )->get();
 	}
