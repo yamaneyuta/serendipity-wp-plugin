1a91264cfa5dcd6f805e4088b7748c349cb4fc0c
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-24 09:49:45 +0000

    change: フロントエンド側でブロックの属性のデフォルトをnullに変更

--- src/block/types/WidgetAttributes.ts
+++ src/block/types/WidgetAttributes.ts
@@ -1,16 +1,17 @@
 /**
  * ブロックの属性(`wp_post`テーブルの`post_content`に保存されるHTMLのコメントに含まれる値)
  * (BlockAttributesは`@wordpress/blocks`に存在したため別の名称で定義)
+ * ※ src/block/index.tsと型の同期をとること。
  */
 export type WidgetAttributes = {
 	/** 販売対象のネットワークカテゴリID */
 	sellingNetworkCategoryID: number | null;
 
 	/** 販売価格の値(sellingDecimalsの値と共に使用する) */
-	sellingAmountHex: string;
+	sellingAmountHex: string | null;
 
 	/** 販売価格の小数点以下桁数 */
-	sellingDecimals: number;
+	sellingDecimals: number | null;
 
 	/** 販売価格の通貨シンボル */
 	sellingSymbol: string | null;
