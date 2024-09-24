d12a9a55fb23cf2fb9142aec55557268d510558c
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-24 05:49:55 +0000

    change: schema.graphqlの場所を変更

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-	schema: './includes/assets/graphql/schema.graphql',
+	schema: './includes/assets/graphql/schema/schema.graphql',
 	documents: [ './includes/assets/graphql/block/*.graphql' ],
 	ignoreNoDocuments: true, // for better experience with the watcher
 	generates: {
