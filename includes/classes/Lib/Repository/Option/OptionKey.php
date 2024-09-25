a7b501a64331741fe876f6e6cdbbd88a62ae471d
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-25 12:09:17 +0000

    docs: OptionKeyは将来的に使用しなくなるためコメントを追記

--- includes/classes/Lib/Repository/Option/OptionKey.php
+++ includes/classes/Lib/Repository/Option/OptionKey.php
@@ -7,10 +7,18 @@ use Cornix\Serendipity\Core\Lib\SystemInfo\PluginInfo;
 
 final class OptionKey {
 
-	/** データベーススキーマバージョンを保存するオプションキー */
+	/**
+	 * データベーススキーマバージョンを保存するオプションキー
+	 *
+	 * @deprecated データベースのスキーマバージョンは管理しない。プラグインのバージョンで管理する。
+	 */
 	public const DB_SCHEMA_VERSION = 'db_schema_version';
 
-	/** プラグインの設定を保存するオプションキーのプレフィックスを取得します。 */
+	/**
+	 * プラグインの設定を保存するオプションキーのプレフィックスを取得します。
+	 *
+	 * @deprecated TODO: OptionPrefixクラスを作成し、このメソッドを移動する
+	 */
 	public static function prefix(): string {
 		$prefix = ( new PluginInfo() )->optionNamePrefix();
 		assert( ! empty( $prefix ) );
