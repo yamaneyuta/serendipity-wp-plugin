7e2fd438434b3cf730d560a80a3bf1a4a6b12113
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-25 12:44:21 +0000

    add: 署名用ウォレットの秘密鍵が存在しない場合はoptionsテーブルに保存する処理を追加

--- includes/classes/Features/Update/Version/0.0.1.php
+++ includes/classes/Features/Update/Version/0.0.1.php
@@ -3,16 +3,34 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Features\Update\Version;
 
+use Cornix\Serendipity\Core\Lib\Repository\SignerPrivateKey;
+use Cornix\Serendipity\Core\Lib\Web3\PrivateKey;
+
 /**
  * Ver0.0.1(インストール直後に実行されるように一番小さいバージョンで仮作成)
  */
 class v001 {
 
 	public function up() {
-		// Do Nothing
+		// 署名用ウォレットの秘密鍵が存在しない場合は生成して保存
+		( new PrivateKeyInitializer() )->initialize();
 	}
 
 	public function down() {
-		// Do Nothing
+		// 署名用ウォレットの秘密鍵の削除は行わない
+	}
+}
+
+
+class PrivateKeyInitializer {
+	/**
+	 * 署名用ウォレットの秘密鍵が存在しない場合は生成して保存します。
+	 */
+	public function initialize(): void {
+		$private_key = ( new SignerPrivateKey() )->get();
+		if ( null === $private_key ) {
+			$private_key = ( new PrivateKey() )->generate();
+			( new SignerPrivateKey() )->save( $private_key );
+		}
 	}
 }
