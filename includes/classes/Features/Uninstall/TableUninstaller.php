2ca06917dc31c15c156e95ce8a088296927f1fff
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-09-27 05:44:55 +0000

    add: プラグインが使用するテーブルを削除するためのクラスを追加

---
+++ includes/classes/Features/Uninstall/TableUninstaller.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Features\Uninstall;
+
+use Cornix\Serendipity\Core\Lib\Database\MySQLiFactory;
+use Cornix\Serendipity\Core\Lib\Repository\Prefix;
+use Cornix\Serendipity\Core\Lib\Strings\Strings;
+
+class TableUninstaller {
+	public function execute( \wpdb $wpdb ): void {
+		$table_name_prefix = ( new Prefix() )->tableName();
+
+		// $table_name_prefixで始まるテーブルをすべて削除する
+		$sql = <<<SQL
+			SHOW TABLES LIKE '{$table_name_prefix}%'
+		SQL;
+
+		$table_names = $wpdb->get_col( $sql );
+
+		// $table_namesの数が多い時はエラー
+		// ※テーブルが追加された時はここがエラーになるので値を修正すること
+		if ( 1 < count( $table_names ) ) {
+			throw new \Exception( '[D6CACCCC] Invalid table names. table_names: ' . json_encode( $table_names ) );
+		}
+
+		foreach ( $table_names as $table_name ) {
+			assert( 0 === Strings::strpos( $table_name, $table_name_prefix ) );
+			$mysqli = ( new MySQLiFactory() )->create( $wpdb );
+			$result = $mysqli->query( "DROP TABLE IF EXISTS `$table_name`" );
+			assert( false !== $result );
+		}
+	}
+}
