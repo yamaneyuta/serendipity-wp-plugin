e64e2755e44c3237976e5725e43747fbd002f502
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-10-10 12:06:31 +0000

    add: 動作モードが開発モードかどうかを取得するメソッドを追加

--- includes/classes/Lib/Repository/Environment.php
+++ includes/classes/Lib/Repository/Environment.php
@@ -3,10 +3,29 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Lib\Repository;
 
+use Cornix\Serendipity\Core\Lib\Repository\Option\OptionFactory;
+
 /**
  * インストールされている環境から情報を取得するクラス。
  * マシンに配置されているファイルやインストール済みのデータベースなど、実行環境によって異なる情報を取得する場合に使用します。
  */
 class Environment {
 
+	/**
+	 * 開発モードかどうかを取得します。
+	 */
+	public function isDevelopmentMode(): bool {
+		$option = ( new OptionFactory() )->isDevelopmentMode();
+		/** @var string|null */
+		$is_development_mode = $option->get( null );
+		$is_development_mode = is_null( $is_development_mode ) ? null : (bool) $is_development_mode;
+
+		if ( is_null( $is_development_mode ) ) {
+			$package_json_path   = __DIR__ . '/../../../../package.json';
+			$is_development_mode = file_exists( $package_json_path );
+			$option->update( $is_development_mode );    // '0'や'1'のような文字列で保存される
+		}
+
+		return $is_development_mode;
+	}
 }
