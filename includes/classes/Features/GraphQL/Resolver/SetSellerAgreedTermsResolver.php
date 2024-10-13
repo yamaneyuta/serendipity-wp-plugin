4ebfe29dec4eebc923e1d320e578257ab3e7acc9
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-10-13 11:42:37 +0000

    add: 販売者が利用規約に同意したときの情報を保存するResolverを追加

---
+++ includes/classes/Features/GraphQL/Resolver/SetSellerAgreedTermsResolver.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Features\GraphQL\Resolver;
+
+use Cornix\Serendipity\Core\Lib\Repository\SellerAgreedTerms;
+use Cornix\Serendipity\Core\Lib\Security\Judge;
+
+class SetSellerAgreedTermsResolver extends ResolverBase {
+
+	/**
+	 * #[\Override]
+	 *
+	 * @return bool
+	 */
+	public function resolve( array $root_value, array $args ) {
+		/** @var int */
+		$version = $args['version'];
+		/** @var string */
+		$message = $args['message'];
+		/** @var string */
+		$signature = $args['signature'];
+
+		// 管理者権限を持っているかどうかをチェック
+		Judge::checkIsAdministrator();
+
+		// 販売者の署名情報を保存
+		( new SellerAgreedTerms() )->save( $version, $message, $signature );
+
+		return true;
+	}
+}
