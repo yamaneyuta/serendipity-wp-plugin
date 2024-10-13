0e2c81c541e4d76ed62498ca24488294a314c2ed
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-10-13 11:34:14 +0000

    add: 販売者の利用規約情報を取得または設定するメソッドを追加

--- includes/classes/Lib/Repository/Option/OptionFactory.php
+++ includes/classes/Lib/Repository/Option/OptionFactory.php
@@ -10,9 +10,18 @@ class OptionFactory {
 
 	/**
 	 * Optionオブジェクトを生成します。
+	 *
+	 * @deprecated
 	 */
 	private function createOption( string $raw_option_key_name ): Option {
 		return new Option( ( new Prefix() )->optionKeyName() . $raw_option_key_name );
+	}
+
+	/**
+	 * optionsテーブルに問い合わせる時のキーを取得します。
+	 */
+	private function getOptionKeyName( string $raw_option_key_name ): string {
+		return ( new Prefix() )->optionKeyName() . $raw_option_key_name;
 	}
 
 	/**
@@ -55,5 +64,26 @@ class OptionFactory {
 	 */
 	public function isDevelopmentMode(): Option {
 		return $this->createOption( 'is_development_mode' );
+	}
+
+	/**
+	 * 販売者が同意した利用規約のバージョンを取得または保存するオブジェクトを取得します。
+	 */
+	public function sellerAgreedTermsVersion(): IntOption {
+		return new IntOption( $this->getOptionKeyName( 'seller_agreed_terms_version' ) );
+	}
+
+	/**
+	 * 販売者が署名した同意メッセージを取得または保存するオブジェクトを取得します。
+	 */
+	public function sellerAgreedTermsMessage(): StringOption {
+		return new StringOption( $this->getOptionKeyName( 'seller_agreed_terms_message' ) );
+	}
+
+	/**
+	 * 販売者が利用規約に同意した際の署名を取得または保存するオブジェクトを取得します。
+	 */
+	public function sellerAgreedTermsSignature(): StringOption {
+		return new StringOption( $this->getOptionKeyName( 'seller_agreed_terms_signature' ) );
 	}
 }
