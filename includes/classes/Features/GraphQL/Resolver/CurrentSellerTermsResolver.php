eb6d209b55c3bf64b510df7bb641f23838131723
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-10-13 12:01:17 +0000

    add: 現在の販売者向け利用規約に関する情報を返すResolverを追加

---
+++ includes/classes/Features/GraphQL/Resolver/CurrentSellerTermsResolver.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Features\GraphQL\Resolver;
+
+use Cornix\Serendipity\Core\Lib\Repository\SellerTerms;
+
+class CurrentSellerTermsResolver extends ResolverBase {
+
+	/**
+	 * #[\Override]
+	 *
+	 * @return array
+	 */
+	public function resolve( array $root_value, array $args ) {
+		// 最新の販売者向け利用規約の情報を取得
+		$seller_terms = new SellerTerms();
+		return array(
+			'version' => $seller_terms->version(),
+			'message' => $seller_terms->message( $seller_terms->version() ),
+		);
+	}
+}
