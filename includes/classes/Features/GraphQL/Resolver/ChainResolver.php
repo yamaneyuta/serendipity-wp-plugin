ecf97bccc9eed3a53ea7967593e0840fa0e0ee38
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-10-15 06:06:43 +0000

    add: Chainクエリのプロパティに`payableTokens`を追加

--- includes/classes/Features/GraphQL/Resolver/ChainResolver.php
+++ includes/classes/Features/GraphQL/Resolver/ChainResolver.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Features\GraphQL\Resolver;
 
+use Cornix\Serendipity\Core\Lib\Repository\PayableTokens;
+use Cornix\Serendipity\Core\Lib\Security\Judge;
+
 class ChainResolver extends ResolverBase {
 
 	/**
@@ -12,10 +15,29 @@ class ChainResolver extends ResolverBase {
 	 */
 	public function resolve( array $root_value, array $args ) {
 		/** @var int */
-		$chain_id = $args['chainID'];
+		$chain_ID = $args['chainID'];
+
+		$payable_tokens_callback = function () use ( $root_value, $chain_ID ) {
+			Judge::checkHasAdminRole(); // 管理者権限が必要
+			$payable_tokens = ( new PayableTokens() )->get( $chain_ID );
+
+			return array_map(
+				function ( $token ) use ( $root_value ) {
+					return $root_value['token'](
+						$root_value,
+						array(
+							'chainID' => $token->chainID(),
+							'address' => $token->address(),
+						)
+					);
+				},
+				$payable_tokens
+			);
+		};
 
 		return array(
-			'id' => $chain_id,
+			'id'            => $chain_ID,
+			'payableTokens' => $payable_tokens_callback,
 		);
 	}
 }
