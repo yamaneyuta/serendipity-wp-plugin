44fb8081ff6926799fd71205ffb7aeb5e268dd5d
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-10-18 02:30:37 +0000

    add: チェーンの定義を追加

--- includes/classes/Lib/Repository/Constants/ChainID.php
+++ includes/classes/Lib/Repository/Constants/ChainID.php
@@ -5,16 +5,29 @@ namespace Cornix\Serendipity\Core\Lib\Repository\Constants;
 
 final class ChainID {
 	// ==================== Mainnet ====================
-	/** イーサリアムメインネット */
+	/** イーサリアムメインネット(L1) */
 	public const ETH_MAINNET = 1;
 
+
+	/** Polygon zkEVM(L2/mainnet) */
+	public const POLYGON_ZK_EVM = 1101; // 0x44d
+
 	// ==================== Testnet ====================
-	/** イーサリアムSepoliaテストネット */
-	public const SEPOLIA = 11155111;
+	/** イーサリアムSepoliaテストネット(L1) */
+	public const SEPOLIA = 11155111;    // 0xaa36a7
+
+
+	/** Polygon zkEVMテストネット(L2/Sepolia) */
+	public const POLYGON_ZK_EVM_CARDONA = 2442; // 0x98a
+
+	/** Soneiumテストネット(L2/Sepolia) */
+	public const SONEIUM_MINATO = 1946; // 0x79a
 
 	// ==================== Privatenet ====================
 	/** PrivatenetL1に位置付けられたチェーンID */
-	public const PRIVATENET_L1 = 31337;
-	/** PrivatenetL2に位置付けられたチェーンID */
-	public const PRIVATENET_L2 = 1337;
+	public const PRIVATENET_L1 = 31337; // 0x7a69
+
+
+	/** PrivatenetL2に位置付けられたチェーンID(L2) ※実際はロールアップを行っていない、単に独立したネットワーク */
+	public const PRIVATENET_L2 = 1337;  // 0x539
 }
