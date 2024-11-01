746639c01ad9cad9f880e2583eea63b32653bafe
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-11-01 10:36:51 +0000

    change: BuiltInRpcUrlDefinition内部の定義を配列からクラスオブジェクトに変更

--- includes/classes/Lib/Repository/Definition/BuiltInRpcUrlDefinition.php
+++ includes/classes/Lib/Repository/Definition/BuiltInRpcUrlDefinition.php
@@ -13,15 +13,7 @@ use Cornix\Serendipity\Core\Lib\Repository\Environment;
  * 組み込みのRPC URL定義
  */
 class BuiltInRpcUrlDefinition {
-
-	private const CHAIN_ID_INDEX = 0;
-	private const RPC_URL_INDEX  = 1;
-
 	public function __construct() {
-		// 参考:
-		// - https://www.ankr.com/rpc/
-		// - https://publicnode.com/
-		// - https://tokenswap.exchange/tools/chainlist
 
 		// プライベートネットのURLを取得する関数
 		$privatenet = function ( int $number ): string {
@@ -30,41 +22,66 @@ class BuiltInRpcUrlDefinition {
 			return "http://{$prefix}privatenet-{$number}.local";
 		};
 
-		$this->built_in_rpc_data = array(
-			// `https://cloudflare-eth.com`はGitHub Actionsでのテストが通らないため、コメントアウト
-			array( ChainID::ETH_MAINNET, 'https://rpc.ankr.com/eth' ),              // https://www.ankr.com/rpc/eth/
-			// array( ChainID::ETH_MAINNET, 'https://cloudflare-eth.com' ),            // https://developers.cloudflare.com/web3/ethereum-gateway/reference/supported-networks/
-			array( ChainID::ETH_MAINNET, 'https://ethereum-rpc.publicnode.com' ),   // https://ethereum.publicnode.com/
+		// 参考:
+		// - https://www.ankr.com/rpc/
+		// - https://publicnode.com/
+		// - https://tokenswap.exchange/tools/chainlist
+		$this->data = array(
+			new RpcUrlData( ChainID::ETH_MAINNET, 'https://rpc.ankr.com/eth' ),              // https://www.ankr.com/rpc/eth/
+			// ↓`https://cloudflare-eth.com`はGitHub Actionsでのテストが通らないため、コメントアウト
+			// new RpcUrlData(  ChainID::ETH_MAINNET, 'https://cloudflare-eth.com' ),            // https://developers.cloudflare.com/web3/ethereum-gateway/reference/supported-networks/
+			new RpcUrlData( ChainID::ETH_MAINNET, 'https://ethereum-rpc.publicnode.com' ),   // https://ethereum.publicnode.com/
 
-			array( ChainID::POLYGON_ZK_EVM, 'https://zkevm-rpc.com' ),              // * https://support.polygon.technology/support/solutions/articles/82000893127-how-to-add-zkevm-network-to-metamask-
-			array( ChainID::POLYGON_ZK_EVM, 'https://rpc.ankr.com/polygon_zkevm' ), // https://www.ankr.com/rpc/polygon_zkevm/
+			new RpcUrlData( ChainID::POLYGON_ZK_EVM, 'https://zkevm-rpc.com' ),              // * https://support.polygon.technology/support/solutions/articles/82000893127-how-to-add-zkevm-network-to-metamask-
+			new RpcUrlData( ChainID::POLYGON_ZK_EVM, 'https://rpc.ankr.com/polygon_zkevm' ), // https://www.ankr.com/rpc/polygon_zkevm/
 
-			array( ChainID::SEPOLIA, 'https://rpc.ankr.com/eth_sepolia' ),              // https://www.ankr.com/rpc/eth/
-			array( ChainID::SEPOLIA, 'https://ethereum-sepolia-rpc.publicnode.com' ),   // https://ethereum.publicnode.com/?sepolia
+			new RpcUrlData( ChainID::SEPOLIA, 'https://rpc.ankr.com/eth_sepolia' ),              // https://www.ankr.com/rpc/eth/
+			new RpcUrlData( ChainID::SEPOLIA, 'https://ethereum-sepolia-rpc.publicnode.com' ),   // https://ethereum.publicnode.com/?sepolia
 
-			array( ChainID::POLYGON_ZK_EVM_CARDONA, 'https://rpc.cardona.zkevm-rpc.com' ),          // * https://docs.polygon.technology/zkEVM/get-started/quick-start/#manually-add-network-to-wallet
-			array( ChainID::POLYGON_ZK_EVM_CARDONA, 'https://rpc.ankr.com/polygon_zkevm_cardona' ), // https://www.ankr.com/rpc/polygon_zkevm/
+			new RpcUrlData( ChainID::POLYGON_ZK_EVM_CARDONA, 'https://rpc.cardona.zkevm-rpc.com' ),          // * https://docs.polygon.technology/zkEVM/get-started/quick-start/#manually-add-network-to-wallet
+			new RpcUrlData( ChainID::POLYGON_ZK_EVM_CARDONA, 'https://rpc.ankr.com/polygon_zkevm_cardona' ), // https://www.ankr.com/rpc/polygon_zkevm/
 
-			array( ChainID::SONEIUM_MINATO, 'https://rpc.minato.soneium.org' ), // * https://docs.soneium.org/docs/builders/overview
+			new RpcUrlData( ChainID::SONEIUM_MINATO, 'https://rpc.minato.soneium.org' ), // * https://docs.soneium.org/docs/builders/overview
 
-			array( ChainID::PRIVATENET_L1, $privatenet( 1 ) ),
-			array( ChainID::PRIVATENET_L2, $privatenet( 2 ) ),
+			new RpcUrlData( ChainID::PRIVATENET_L1, $privatenet( 1 ) ),
+			new RpcUrlData( ChainID::PRIVATENET_L2, $privatenet( 2 ) ),
 		);
 	}
 
-	/** @var [int, string][] */
-	private array $built_in_rpc_data;
+	/** @var RpcUrlData[] */
+	private array $data;
 
 	/**
 	 * 指定したチェーンIDに対応する組み込みのRPC URL一覧を取得します。
 	 */
 	public function getUrls( int $chain_ID ): array {
-		$rpc_urls = array();
-		foreach ( $this->built_in_rpc_data as $rpc_data ) {
-			if ( $rpc_data[ self::CHAIN_ID_INDEX ] === $chain_ID ) {
-				$rpc_urls[] = $rpc_data[ self::RPC_URL_INDEX ];
-			}
-		}
-		return $rpc_urls;
+
+		$filtered = array_filter(
+			$this->data,
+			fn( RpcUrlData $rpc_data ) => $rpc_data->chainID() === $chain_ID
+		);
+
+		return array_map(
+			fn( RpcUrlData $rpc_data ) => $rpc_data->rpcURL(),
+			array_values( $filtered )
+		);
+	}
+}
+
+/** @internal */
+class RpcUrlData {
+	public function __construct( int $chain_ID, string $rpc_url ) {
+		$this->chain_ID = $chain_ID;
+		$this->rpc_url  = $rpc_url;
+	}
+	private int $chain_ID;
+	private string $rpc_url;
+
+	public function chainID(): int {
+		return $this->chain_ID;
+	}
+
+	public function rpcURL(): string {
+		return $this->rpc_url;
 	}
 }
