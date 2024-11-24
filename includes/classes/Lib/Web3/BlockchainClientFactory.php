f37800e5d0a16742d7855974e83f253806ec954e
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-11-24 12:37:14 +0000

    add: ブロックチェーンに接続するためのインスタンスを生成するクラスを追加

---
+++ includes/classes/Lib/Web3/BlockchainClientFactory.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Lib\Web3;
+
+use Cornix\Serendipity\Core\Lib\Repository\RpcURL;
+
+class BlockchainClientFactory {
+	/**
+	 * 指定したチェーンに接続するオブジェクトを生成します。
+	 */
+	public function create( int $chain_ID ): Blockchain {
+		// チェーンに接続するためのRPC URLを取得
+		$rpc_url = ( new RpcURL() )->connectableURL( $chain_ID );
+		if ( is_null( $rpc_url ) ) {
+			throw new \Exception( '[4513DF1F] RPC URL is not found.' );
+		}
+
+		return new Blockchain( $rpc_url );
+	}
+}
