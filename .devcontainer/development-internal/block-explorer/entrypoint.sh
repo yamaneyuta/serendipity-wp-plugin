97be5c423142086e87f54ebd7f4fa3bbeca729ae
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-11-25 04:22:24 +0000

    add: block-explorerをプライベートネット用に作成

---
+++ .devcontainer/development-internal/block-explorer/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# `block-explorer`ディレクトリが存在しない場合はclone
+# 存在する場合は最新の状態に更新
+if [ ! -d block-explorer ]; then
+  echo "clone repository"
+  git clone https://github.com/Shaivpidadi/block-explorer
+  cd block-explorer
+else
+	echo "update repository"
+  cd block-explorer
+  # git pull
+	git fetch origin main
+	git reset --hard origin/main
+fi
+
+npm install
+npm run start
