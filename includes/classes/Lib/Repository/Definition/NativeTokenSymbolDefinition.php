c95e99133fb084016fd8b0083fc46dd0b756739c
山根 佑太 <yamaneyuta@users.noreply.github.com>
2024-12-12 02:17:13 +0000

    add: ネイティブトークンのシンボルを取得するクラス

---
+++ includes/classes/Lib/Repository/Definition/NativeTokenSymbolDefinition.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Lib\Repository\Definition;
+
+use Cornix\Serendipity\Core\Lib\Repository\Constants\ChainID;
+
+class NativeTokenSymbolDefinition {
+	/**
+	 * 対象のチェーンIDで使用されているネイティブトークンのシンボルを取得します。
+	 */
+	public function get( int $chain_ID ): string {
+		switch ( $chain_ID ) {
+			case ChainID::ETH_MAINNET:
+			case ChainID::POLYGON_ZK_EVM:
+			case ChainID::SEPOLIA:
+			case ChainID::POLYGON_ZK_EVM_CARDONA:
+			case ChainID::SONEIUM_MINATO:
+			case ChainID::PRIVATENET_L1:
+				return 'ETH';
+			case ChainID::PRIVATENET_L2:
+				return 'MATIC'; // TODO: OracleでPOL/USD等が取得できるようになったタイミングでPOLへ変更
+			default:
+				throw new \InvalidArgumentException( '[398C040E] Invalid chain ID. - ' . $chain_ID );
+		}
+	}
+}
