36d6091a9e5a1106d420fea07baa1a65054eb361
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-01-21 02:26:47 +0000

    refactor: RpcURL class
    指定したチェーンに接続するRPC URLを取得する処理をユーザーが指定したRPC URLか組み込みのRPC URLを返すように処理を変更

--- includes/classes/Lib/Repository/RpcURL.php
+++ includes/classes/Lib/Repository/RpcURL.php
@@ -3,109 +3,39 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Lib\Repository;
 
-use Cornix\Serendipity\Core\Lib\Repository\Definition\RpcURL\AnkrRpcUrlDefinition;
-use Cornix\Serendipity\Core\Lib\Repository\Definition\RpcURL\PolygonLabsRpcUrlDefinition;
-use Cornix\Serendipity\Core\Lib\Repository\Definition\RpcURL\PrivatenetRpcUrlDefinition;
-use Cornix\Serendipity\Core\Lib\Repository\Definition\RpcURL\PublicNodeRpcUrlDefinition;
-use Cornix\Serendipity\Core\Lib\Repository\Definition\RpcURL\RpcUrlDefinitionBase;
-use Cornix\Serendipity\Core\Lib\Repository\Definition\RpcURL\RpcComRpcUrlDefinition;
-use Cornix\Serendipity\Core\Lib\Repository\Definition\RpcURL\SoneiumRpcUrlDefinition;
+use Cornix\Serendipity\Core\Lib\Repository\Definition\BuiltInRPC\BuiltInRpcProviderDefinition;
+use Cornix\Serendipity\Core\Lib\Repository\Definition\BuiltInRPC\BuiltInRpcUrlDefinition;
+use Cornix\Serendipity\Core\Lib\Security\Judge;
 
 class RpcURL {
 
-	public function __construct() {
-		// 仮で組み込みのRPC URLを登録
-		// TODO: ユーザーが利用規約に同意したかどうかの情報を取得
-		$third_party_rpc_url_defs   = array();
-		$third_party_rpc_url_defs[] = new BuiltInRpcURL( new SoneiumRpcUrlDefinition(), true );
-		$third_party_rpc_url_defs[] = new BuiltInRpcURL( new PolygonLabsRpcUrlDefinition(), true );
-		$third_party_rpc_url_defs[] = new BuiltInRpcURL( new AnkrRpcUrlDefinition(), true );
-		$third_party_rpc_url_defs[] = new BuiltInRpcURL( new RpcComRpcUrlDefinition(), true );
-		$third_party_rpc_url_defs[] = new BuiltInRpcURL( new PublicNodeRpcUrlDefinition(), true );
-
-		// プライベートネット用のRPC URL
-		$privatenet_rpc_url_defs   = array();
-		$privatenet_rpc_url_defs[] = new class() implements IRpcURL {
-			public function connectableURL( int $chain_ID ): ?string {
-				return ( new PrivatenetRpcUrlDefinition() )->get( $chain_ID );
-			}
-		};
-
-		// すべてのRPC URL定義をマージ
-		$this->all_rpc_url_defs = array_merge(
-			$third_party_rpc_url_defs,  // 組み込みのRPC URL
-			$privatenet_rpc_url_defs,   // プライベートネット接続用のRPC URL
-		);
-	}
-	private array $all_rpc_url_defs = array();
-
 	/**
-	 * 指定したチェーンに接続できるRPC URL一覧を取得します。
+	 * 指定したチェーンに接続するためのRPC URLを取得します。
 	 *
 	 * @param int $chain_ID
-	 * @return string[]
 	 */
-	public function allConnectableURL( int $chain_ID ): array {
-		$result = array();
-		foreach ( $this->all_rpc_url_defs as $rpc_url_def ) {
-			$url = $rpc_url_def->connectableURL( $chain_ID );
-			if ( ! is_null( $url ) ) {
-				$result[] = $url;
+	public function get( int $chain_ID ): ?string {
+		$user_settings = new RpcUserSettings();
+		if ( $user_settings->isUseCustomRpcUrl( $chain_ID ) ) {
+			// ユーザーが設定したRPC URLを使用する場合
+			// 設定されたRPC URLを取得して返す
+			assert( Judge::isUrl( $user_settings->getRpcURL( $chain_ID ) ) );
+			return $user_settings->getRpcURL( $chain_ID );
+		} else {
+			// ユーザーが設定したRPC URLを使用しない場合
+			// 組み込みのRPC URLを返すが、利用規約に同意していない場合はnullを返す
+
+			// 指定されたチェーンで接続するRPCプロバイダを取得
+			$rpc_provider = ( new BuiltInRpcProviderDefinition() )->get( $chain_ID );
+
+			if ( $user_settings->isAgreedTerms( $rpc_provider ) ) {
+				// 利用規約に同意している場合は組み込みのRPC URLを返す
+				// ※ nullの場合もあることに注意
+				return ( new BuiltInRpcUrlDefinition() )->get( $rpc_provider, $chain_ID );
+			} else {
+				// 利用規約に同意していない場合はnullを返す
+				return null;
 			}
 		}
-		return $result;
-	}
-
-	/**
-	 * 指定したチェーンに接続できるRPC URLを取得します。
-	 */
-	public function connectableURL( int $chain_ID ): ?string {
-		foreach ( $this->all_rpc_url_defs as $rpc_url_def ) {
-			$url = $rpc_url_def->connectableURL( $chain_ID );
-			if ( ! is_null( $url ) ) {
-				return $url;
-			}
-		}
-		return null;
-	}
-}
-
-interface IRpcURL {
-	/**
-	 * 指定したチェーンに接続できるRPC URLを取得します。
-	 */
-	public function connectableURL( int $chain_ID ): ?string;
-}
-
-/**
- * ユーザーが設定したRPC URLに関する情報を取得するクラス
- *
- * @internal
- */
-class UserSettingsRpcURL implements IRpcURL {
-	public function connectableURL( int $chain_ID ): ?string {
-		// TODO: 実装
-		throw new \Exception( '[A3A82122] UserSettingsRpcURL::connectableURL() - Not implemented' );
-	}
-}
-
-
-/**
- * このプラグインで定義している組み込みのRPC URLに関する情報を取得するクラス
- *
- * @internal
- */
-class BuiltInRpcURL implements IRpcURL {
-	public function __construct( RpcUrlDefinitionBase $definition, bool $is_terms_agreed ) {
-		$this->definition      = $definition;
-		$this->is_terms_agreed = $is_terms_agreed;
-	}
-	private RpcUrlDefinitionBase $definition;
-	private bool $is_terms_agreed;
-
-	public function connectableURL( int $chain_ID ): ?string {
-		// RPC URLを取得
-		// ※ 利用規約に同意していない場合はnullを返す
-		return $this->is_terms_agreed ? $this->definition->get( $chain_ID ) : null;
 	}
 }
