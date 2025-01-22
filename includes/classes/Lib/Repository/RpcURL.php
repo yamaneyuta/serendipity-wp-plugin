6be5c2576cd79a0264af653195ff89f5942e9a32
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-01-22 07:12:36 +0000

    refactor: RpcURL
    テストを実施しやすい様にRpcUserSettingsインスタンスを注入できるようにコンストラクタを定義

--- includes/classes/Lib/Repository/RpcURL.php
+++ includes/classes/Lib/Repository/RpcURL.php
@@ -9,18 +9,23 @@ use Cornix\Serendipity\Core\Lib\Security\Judge;
 
 class RpcURL {
 
+	public function __construct( RpcUserSettings $rpc_user_settings = null ) {
+		$this->rpc_user_settings = $rpc_user_settings ?? new RpcUserSettings();
+	}
+
+	private RpcUserSettings $rpc_user_settings;
+
 	/**
 	 * 指定したチェーンに接続するためのRPC URLを取得します。
 	 *
 	 * @param int $chain_ID
 	 */
 	public function get( int $chain_ID ): ?string {
-		$user_settings = new RpcUserSettings();
-		if ( $user_settings->isUseCustomRpcUrl( $chain_ID ) ) {
+		if ( $this->rpc_user_settings->isUseCustomRpcUrl( $chain_ID ) ) {
 			// ユーザーが設定したRPC URLを使用する場合
 			// 設定されたRPC URLを取得して返す
-			assert( Judge::isUrl( $user_settings->getRpcURL( $chain_ID ) ) );
-			return $user_settings->getRpcURL( $chain_ID );
+			assert( Judge::isUrl( $this->rpc_user_settings->getRpcURL( $chain_ID ) ) );
+			return $this->rpc_user_settings->getRpcURL( $chain_ID );
 		} else {
 			// ユーザーが設定したRPC URLを使用しない場合
 			// 組み込みのRPC URLを返すが、利用規約に同意していない場合はnullを返す
@@ -28,7 +33,7 @@ class RpcURL {
 			// 指定されたチェーンで接続するRPCプロバイダを取得
 			$rpc_provider = ( new BuiltInRpcProviderDefinition() )->get( $chain_ID );
 
-			if ( $user_settings->getIsAgreedTerms( $rpc_provider ) ) {
+			if ( $this->rpc_user_settings->getIsAgreedTerms( $rpc_provider ) ) {
 				// 利用規約に同意している場合は組み込みのRPC URLを返す
 				// ※ nullの場合もあることに注意
 				return ( new BuiltInRpcUrlDefinition() )->get( $rpc_provider, $chain_ID );
