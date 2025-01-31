c8dd1d21f3f73495dad1916c135dc0f1a2bc91a2
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-01-31 01:10:10 +0000

    add: oracleテーブルに初期値を設定する処理
    インストール時にoracleテーブルにレコード０を追加する

--- includes/classes/Features/Update/Version/0.0.1.php
+++ includes/classes/Features/Update/Version/0.0.1.php
@@ -47,6 +47,9 @@ class v001 {
 		( new UnlockPaywallTransactionTable( $wpdb ) )->create();
 		// ペイウォール解除時のトークン転送イベントの内容を記録するテーブルを作成
 		( new UnlockPaywallTransferEventTable( $wpdb ) )->create();
+
+		// oracleテーブルの初期値を設定
+		( new OracleTableRecordInitializer( $wpdb ) )->initialize();
 	}
 
 	public function down() {
@@ -166,5 +169,40 @@ class PrivateKeyInitializer {
 			$private_key = ( new PrivateKey() )->generate();
 			$server_signer_data->save( $private_key );
 		}
+	}
+}
+
+
+class OracleTableRecordInitializer {
+	private $wpdb;
+
+	public function __construct( $wpdb ) {
+		$this->wpdb = $wpdb;
+	}
+
+	/**
+	 * Oracleテーブルの初期値を設定します。
+	 */
+	public function initialize(): void {
+		$oracle_table = new OracleTable( $this->wpdb );
+
+		// メインネットのOracleを登録
+		// Fiatはメインネットに存在するものをすべて登録(2025/1/30時点)、CryptoはETH/USDのみ(動作する最低限)を登録
+		// ※ 最新の情報をWEBから取得して登録することはしない(WEBサイトが停止された場合に1つも登録できなくなる、等のリスクがあるため)
+
+		// Fiat
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0x01435677FB11763550905594A16B645847C1d0F3', 'KRW', 'USD' );
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0x77F9710E7d0A19669A13c055F62cd80d313dF022', 'AUD', 'USD' );
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0x5c0Ab2d9b5a7ed9f470386e82BB36A3613cDd4b5', 'GBP', 'USD' );
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0x449d117117838fFA61263B61dA6301AA2a88B13A', 'CHF', 'USD' );
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0xeF8A4aF35cd47424672E3C590aBD37FBB7A7759a', 'CNY', 'USD' );
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0xb49f677943BC038e9857d61E7d053CaA2C1734C1', 'EUR', 'USD' );
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0xBcE206caE7f0ec07b545EddE332A47C2F75bbeb3', 'JPY', 'USD' );
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0xB09fC5fD3f11Cf9eb5E1C5Dba43114e3C9f477b5', 'TRY', 'USD' );
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0xa34317DB73e77d453b1B8d04550c44D10e981C8e', 'CAD', 'USD' );
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0x3977CFc9e4f29C184D4675f4EB8e0013236e5f3e', 'NZD', 'USD' );
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0xe25277fF4bbF9081C75Ab0EB13B4A13a721f3E13', 'SGD', 'USD' );
+		// Crypto
+		$oracle_table->insert( ChainID::ETH_MAINNET, '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419', 'ETH', 'USD' );
 	}
 }
