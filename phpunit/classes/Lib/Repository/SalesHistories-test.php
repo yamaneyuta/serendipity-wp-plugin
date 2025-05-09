cd6f440a855ec471df55ad24c0f85a8f7478e834
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-05-09 04:59:49 +0000

    UnlockPaywallTransferイベントの引数追加対応 Fixes #189

--- phpunit/classes/Lib/Repository/SalesHistories-test.php
+++ phpunit/classes/Lib/Repository/SalesHistories-test.php
@@ -5,6 +5,9 @@ use Cornix\Serendipity\Core\Lib\Database\Schema\InvoiceTable;
 use Cornix\Serendipity\Core\Lib\Database\Schema\TokenTable;
 use Cornix\Serendipity\Core\Lib\Database\Schema\UnlockPaywallTransactionTable;
 use Cornix\Serendipity\Core\Lib\Database\Schema\UnlockPaywallTransferEventTable;
+use Cornix\Serendipity\Core\Lib\Repository\AppContract;
+use Cornix\Serendipity\Core\Lib\Repository\Constants\ChainID;
+use Cornix\Serendipity\Core\Lib\Repository\Constants\UnlockPaywallTransferType;
 use Cornix\Serendipity\Core\Lib\Repository\Name\TableName;
 use Cornix\Serendipity\Core\Lib\Repository\SalesHistories;
 use Cornix\Serendipity\Core\Lib\Security\Judge;
@@ -121,8 +124,8 @@ class SalesHistoriesTest extends IntegrationTestBase {
 		$sales_test_data->insertTransactionData( '2025-02-07 04:58:04', '01JKFB56B8PQQ261K5VZDCE5DH', '31337', '276', '0x7117fd9b43492484bf18d93a834de4c39ec2e00687ee235594b77129426bb236' );
 
 		// unlock_paywall_transfer_eventテーブルへデータ挿入
-		$sales_test_data->insertTransferEventData( '2025-02-07 04:58:04', '01JKFB56B8PQQ261K5VZDCE5DH', '0', '0x70997970C51812dc3A010C7d01b50e0d17dc79C8', '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318', '0x0000000000000000000000000000000000000000', '0x1610e9a9d064' );
-		$sales_test_data->insertTransferEventData( '2025-02-07 04:58:05', '01JKFB56B8PQQ261K5VZDCE5DH', '1', '0x70997970C51812dc3A010C7d01b50e0d17dc79C8', '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', '0x0000000000000000000000000000000000000000', '0x08888a5cab96f0' );
+		$sales_test_data->insertTransferEventData( '2025-02-07 04:58:04', '01JKFB56B8PQQ261K5VZDCE5DH', '0', '0x70997970C51812dc3A010C7d01b50e0d17dc79C8', '0x5FbDB2315678afecb367f032d93F642f64180aa3', '0x0000000000000000000000000000000000000000', '0x1610e9a9d064', UnlockPaywallTransferType::HANDLING_FEE );
+		$sales_test_data->insertTransferEventData( '2025-02-07 04:58:05', '01JKFB56B8PQQ261K5VZDCE5DH', '1', '0x70997970C51812dc3A010C7d01b50e0d17dc79C8', '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', '0x0000000000000000000000000000000000000000', '0x08888a5cab96f0', UnlockPaywallTransferType::SELLER_PROFIT );
 	}
 
 	/**
@@ -136,16 +139,18 @@ class SalesHistoriesTest extends IntegrationTestBase {
 	 */
 	private function insertTableData2( wpdb $wpdb ): void {
 		$sales_test_data = new SalesTestData( $wpdb );
+		$chain_ID        = ChainID::PRIVATENET_L1;
+		$app_address     = ( new AppContract() )->get( $chain_ID )->address();
 
 		// invoiceテーブルへデータ挿入
-		$sales_test_data->insertInvoiceData( '2025-02-07 10:36:48', '01JKFZQE7YDYVHG97BBXWY4WTJ', '49', '31337', '0x3e8', '0', 'JPY', '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', '0x0000000000000000000000000000000000000000', '0x087f79088eac8e', '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC' );
+		$sales_test_data->insertInvoiceData( '2025-02-07 10:36:48', '01JKFZQE7YDYVHG97BBXWY4WTJ', '49', "${chain_ID}", '0x3e8', '0', 'JPY', '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', '0x0000000000000000000000000000000000000000', '0x087f79088eac8e', '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC' );
 
 		// unlock_paywall_transactionテーブルへデータ挿入
-		$sales_test_data->insertTransactionData( '2025-02-07 10:50:25', '01JKFZQE7YDYVHG97BBXWY4WTJ', '31337', '2068', '0xe6355e851a760d4bb2c283f59fc0cc6af03d983341671ba9789b475ab6d2c4ce' );
+		$sales_test_data->insertTransactionData( '2025-02-07 10:50:25', '01JKFZQE7YDYVHG97BBXWY4WTJ', "${chain_ID}", '2068', '0xe6355e851a760d4bb2c283f59fc0cc6af03d983341671ba9789b475ab6d2c4ce' );
 
 		// unlock_paywall_transfer_eventテーブルへデータ挿入
-		$sales_test_data->insertTransferEventData( '2025-02-07 10:50:25', '01JKFZQE7YDYVHG97BBXWY4WTJ', '0', '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC', '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318', '0x0000000000000000000000000000000000000000', '0x15c135d8777c' );
-		$sales_test_data->insertTransferEventData( '2025-02-07 10:50:25', '01JKFZQE7YDYVHG97BBXWY4WTJ', '1', '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC', '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', '0x0000000000000000000000000000000000000000', '0x0869b7d2b63512' );
+		$sales_test_data->insertTransferEventData( '2025-02-07 10:50:25', '01JKFZQE7YDYVHG97BBXWY4WTJ', '0', '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC', $app_address, '0x0000000000000000000000000000000000000000', '0x15c135d8777c', UnlockPaywallTransferType::HANDLING_FEE );
+		$sales_test_data->insertTransferEventData( '2025-02-07 10:50:25', '01JKFZQE7YDYVHG97BBXWY4WTJ', '1', '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC', '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', '0x0000000000000000000000000000000000000000', '0x0869b7d2b63512', UnlockPaywallTransferType::SELLER_PROFIT );
 	}
 }
 
@@ -219,12 +224,12 @@ class SalesTestData {
 		assert( 1 === $result, '[9811DF9A] ' . $this->wpdb->last_error );
 	}
 
-	public function insertTransferEventData( $created_at, $invoice_id, $log_index, $from_address, $to_address, $token_address, $amount_hex ) {
+	public function insertTransferEventData( $created_at, $invoice_id, $log_index, $from_address, $to_address, $token_address, $amount_hex, $transfer_type ) {
 		$transfer_event_table_name = ( new TableName() )->unlockPaywallTransferEvent();
 		$result                    = $this->wpdb->query(
 			<<<SQL
-				INSERT INTO `{$transfer_event_table_name}` (created_at, invoice_id, log_index, from_address, to_address, token_address, amount_hex)
-				VALUES ('$created_at', '$invoice_id', $log_index, '$from_address', '$to_address', '$token_address', '$amount_hex');
+				INSERT INTO `{$transfer_event_table_name}` (created_at, invoice_id, log_index, from_address, to_address, token_address, amount_hex, transfer_type)
+				VALUES ('$created_at', '$invoice_id', $log_index, '$from_address', '$to_address', '$token_address', '$amount_hex', $transfer_type);
 			SQL
 		);
 		assert( 1 === $result, '[1C46B20E] ' . $this->wpdb->last_error );
