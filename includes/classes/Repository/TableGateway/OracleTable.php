11659978b43edf70837da2b59299a20056606a4f
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-03 06:12:39 +0000

    chore: OracleTable::select -> ::all
    - メソッド名の変更
    - 戻り値をOracleTableRecord[]に変更

--- includes/classes/Repository/TableGateway/OracleTable.php
+++ includes/classes/Repository/TableGateway/OracleTable.php
@@ -6,8 +6,7 @@ namespace Cornix\Serendipity\Core\Repository\TableGateway;
 use Cornix\Serendipity\Core\Lib\Database\MySQLiFactory;
 use Cornix\Serendipity\Core\Repository\Name\TableName;
 use Cornix\Serendipity\Core\Lib\Security\Validate;
-use Cornix\Serendipity\Core\Entity\Oracle;
-use Cornix\Serendipity\Core\ValueObject\Address;
+use Cornix\Serendipity\Core\ValueObject\TableRecord\OracleTableRecord;
 
 /**
  * Oracleの情報を記録するテーブル
@@ -50,41 +49,15 @@ class OracleTable {
 
 	/**
 	 * Oracleデータ一覧を取得します。
-	 * パラメータでチェーンID、アドレス、シンボルを指定することで絞り込みができます。
 	 *
-	 * @param int|null    $chain_ID チェーンID
-	 * @param string|null $address Oracleアドレス
-	 * @param string|null $base_symbol ベースシンボル
-	 * @param string|null $quote_symbol クォートシンボル
-	 * @return Oracle[]
+	 * @return OracleTableRecord[]
 	 */
-	public function select( ?int $chain_ID = null, ?string $address = null, ?string $base_symbol = null, ?string $quote_symbol = null ): array {
+	public function all(): array {
+		// Oracleのデータ量は少ないので絞り込みは上位で行う
 		$sql = <<<SQL
 			SELECT `chain_id`, `address`, `base_symbol`, `quote_symbol`
 			FROM `{$this->table_name}`
 		SQL;
-
-		// 条件がある場合はWHERE句を追加
-		$wheres = array();
-		if ( ! is_null( $chain_ID ) ) {
-			Validate::checkChainID( $chain_ID );
-			$wheres[] = $this->wpdb->prepare( '`chain_id` = %d', $chain_ID );
-		}
-		if ( ! is_null( $address ) ) {
-			$wheres[] = $this->wpdb->prepare( '`address` = %s', $address );
-		}
-		if ( ! is_null( $base_symbol ) ) {
-			Validate::checkSymbol( $base_symbol );
-			$wheres[] = $this->wpdb->prepare( '`base_symbol` = %s', $base_symbol );
-		}
-		if ( ! is_null( $quote_symbol ) ) {
-			Validate::checkSymbol( $quote_symbol );
-			$wheres[] = $this->wpdb->prepare( '`quote_symbol` = %s', $quote_symbol );
-		}
-
-		if ( ! empty( $wheres ) ) {
-			$sql .= ' WHERE ' . implode( ' AND ', $wheres );
-		}
 
 		$result = $this->wpdb->get_results( $sql );
 		if ( false === $result ) {
@@ -93,16 +66,12 @@ class OracleTable {
 
 		$records = array();
 		foreach ( $result as $row ) {
-			$chain_ID     = (int) $row->chain_id;
-			$address      = (string) $row->address;
-			$base_symbol  = (string) $row->base_symbol;
-			$quote_symbol = (string) $row->quote_symbol;
+			$row->chain_id     = (int) $row->chain_id;
+			$row->address      = (string) $row->address;
+			$row->base_symbol  = (string) $row->base_symbol;
+			$row->quote_symbol = (string) $row->quote_symbol;
 
-			assert( Validate::isChainID( $chain_ID ), '[75C4111A] Invalid chain ID. ' . $chain_ID );
-			assert( Validate::isSymbol( $base_symbol ), '[7F884B25] Invalid base symbol. ' . $base_symbol );
-			assert( Validate::isSymbol( $quote_symbol ), '[9A0090FB] Invalid quote symbol. ' . $quote_symbol );
-
-			$records[] = Oracle::from( $chain_ID, Address::from( $address ), $base_symbol, $quote_symbol );
+			$records[] = new OracleTableRecord( $row );
 		}
 
 		return $records;
