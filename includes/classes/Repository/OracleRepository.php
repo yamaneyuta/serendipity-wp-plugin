533344ed5e5e5b5f5dc337d1be07706370a11c7b
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-03 06:13:07 +0000

    add: OracleRepository class

---
+++ includes/classes/Repository/OracleRepository.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Repository;
+
+use Cornix\Serendipity\Core\Entity\Oracles;
+use Cornix\Serendipity\Core\Repository\TableGateway\OracleTable;
+
+class OracleRepository {
+
+	public function __construct( ?OracleTable $oracle_table = null ) {
+		$this->oracle_table = $oracle_table ?? new OracleTable( $GLOBALS['wpdb'] );
+	}
+
+	private OracleTable $oracle_table;
+
+	/**
+	 * データが存在するチェーン一覧(すべて)を取得します。
+	 */
+	public function all(): Oracles {
+		$records = $this->oracle_table->all();
+		return Oracles::fromTableRecords( $records );
+	}
+}
