1af432ad86f12d697892e3aa6e1b63ff1b3924f3
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-05 01:26:55 +0000

    refactor: UnlockPaywallTransferEventTableクラスをTableBaseから継承するように変更

--- includes/classes/Repository/TableGateway/UnlockPaywallTransferEventTable.php
+++ includes/classes/Repository/TableGateway/UnlockPaywallTransferEventTable.php
@@ -3,34 +3,27 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Repository\TableGateway;
 
-use Cornix\Serendipity\Core\Lib\Database\MySQLiFactory;
 use Cornix\Serendipity\Core\Repository\Name\TableName;
 
 
 /**
  * ペイウォール解除イベントのログ
  */
-class UnlockPaywallTransferEventTable {
+class UnlockPaywallTransferEventTable extends TableBase {
 	public function __construct( \wpdb $wpdb ) {
-		$this->wpdb       = $wpdb;
-		$this->mysqli     = ( new MySQLiFactory() )->create( $wpdb );
-		$this->table_name = ( new TableName() )->unlockPaywallTransferEvent();
+		parent::__construct( $wpdb, ( new TableName() )->unlockPaywallTransferEvent() );
 	}
-
-	private \wpdb $wpdb;
-	private \mysqli $mysqli;
-	private string $table_name;
 
 	/**
 	 * テーブルを作成します。
 	 */
 	public function create(): void {
-		$charset    = $this->wpdb->get_charset_collate();
-		$index_name = "idx_{$this->table_name}_E1160E22";
+		$charset    = $this->wpdb()->get_charset_collate();
+		$index_name = "idx_{$this->tableName()}_E1160E22";
 
 		// - 複数回呼び出された時に検知できるように`IF NOT EXISTS`は使用しない
 		$sql = <<<SQL
-			CREATE TABLE `{$this->table_name}` (
+			CREATE TABLE `{$this->tableName()}` (
 				`created_at`     timestamp     NOT NULL DEFAULT CURRENT_TIMESTAMP,
 				`invoice_id`     varchar(191)  NOT NULL,
 				`log_index`      int           NOT NULL,
@@ -44,19 +37,9 @@ class UnlockPaywallTransferEventTable {
 			) {$charset};
 		SQL;
 
-		$result = $this->mysqli->query( $sql );
-		assert( true === $result );
-	}
-
-	/**
-	 * テーブルを削除します。
-	 */
-	public function drop(): void {
-		$sql = <<<SQL
-			DROP TABLE IF EXISTS `{$this->table_name}`;
-		SQL;
-
-		$result = $this->mysqli->query( $sql );
-		assert( true === $result );
+		$result = $this->mysqli()->query( $sql );
+		if ( true !== $result ) {
+			throw new \RuntimeException( '[90A49762] Failed to create unlock paywall transfer event table. ' . $this->mysqli()->error );
+		}
 	}
 }
