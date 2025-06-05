fa2fde13e408a1173c6ace91fb7bd1dcd785e2c6
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-05 02:53:41 +0000

    add: TransactionHash class

---
+++ includes/classes/ValueObject/TransactionHash.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\ValueObject;
+
+class TransactionHash {
+	public function __construct( string $hash ) {
+		$this->hash = $hash;
+	}
+
+	private string $hash;
+
+	/**
+	 * トランザクションハッシュを取得します。
+	 */
+	public function value(): string {
+		return $this->hash;
+	}
+
+	public static function from( string $hash ): self {
+		// フォーマットチェック
+		if ( ! preg_match( '/^0x[a-fA-F0-9]{64}$/', $hash ) ) {
+			throw new \InvalidArgumentException( '[7AF48A4D] Invalid transaction hash format: ' . $hash );
+		}
+
+		return new self( $hash );
+	}
+
+	/**
+	 * トランザクションハッシュを文字列として返します。
+	 */
+	public function __toString(): string {
+		return $this->hash;
+	}
+}
