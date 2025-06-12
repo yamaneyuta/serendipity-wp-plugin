71fee65e8430c273606d3bae5ef38effba2ebeb8
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-12 12:50:44 +0000

    refactor: 開発用のウォレットリセットスクリプトの処理をリファクタ

--- .bin/reset-payment-data.php
+++ .bin/reset-payment-data.php
@@ -8,34 +8,68 @@ require_once __DIR__ . '/../includes/vendor/autoload.php';
  * このスクリプトをpackage.jsonから呼び出されます。
  */
 
-use Cornix\Serendipity\Core\Infrastructure\Database\TableGateway\InvoiceTable;
-use Cornix\Serendipity\Core\Infrastructure\Database\TableGateway\ServerSignerTable;
-use Cornix\Serendipity\Core\Infrastructure\Database\TableGateway\UnlockPaywallTransactionTable;
-use Cornix\Serendipity\Core\Infrastructure\Database\TableGateway\UnlockPaywallTransferEventTable;
+use Cornix\Serendipity\Core\Repository\Environment;
+use Cornix\Serendipity\Core\Repository\Name\Prefix;
+use Cornix\Serendipity\Core\Repository\Name\TableName;
+use Cornix\Serendipity\Core\Service\DatabaseMigrationService;
 use Cornix\Serendipity\Core\Service\Factory\ServerSignerServiceFactory;
 
-// 方針:
-// テストネットで確認することもあるため、コントラクトのデータは変更せず以下の処理を実施
-// - 署名用ウォレットの秘密鍵を新規作成
-// - 請求書や履歴のデータを削除
+// ■ 方針:
+// 署名用ウォレットを再作成し、署名用ウォレットに紐づく情報をすべて削除する
+//
+// ■ 処理フロー:
+// 1. 一旦、不要なデータをすべて削除する
+// 2. テーブルをリネームし、疑似的にテーブルが削除された状態にする
+// 3. マイグレーションを実行して、テーブルを再生成する
+// 4. 署名用ウォレットデータの内容をリネームしたテーブルへ移動
+// 5. マイグレーションで作成されたテーブルをすべて削除し、バックアップのテーブルを元に戻す
 
 global $wpdb;
 
 $server_signer_service = ( new ServerSignerServiceFactory() )->create( $wpdb );
-$prevAddress           = $server_signer_service->getServerSigner()->address();
-( new ServerSignerTable( $wpdb ) )->drop();
-( new ServerSignerTable( $wpdb ) )->create();
-$server_signer_service->initializeServerSigner();
-$newAddress = $server_signer_service->getServerSigner()->address();
 
+// 変更前の署名用ウォレットアドレスを取得(最後に表示するために保持)
+$prevAddress = $server_signer_service->getServerSigner()->address();
+
+// 1. 一旦、不要なデータをすべて削除する
+$delete_table_names = array(
+	( new TableName() )->unlockPaywallTransaction(),
+	( new TableName() )->unlockPaywallTransferEvent(),
+	( new TableName() )->invoice(),
+	( new TableName() )->serverSigner(),
+);
+foreach ( $delete_table_names as $table_name ) {
+	$wpdb->query( "TRUNCATE TABLE `{$table_name}`" );
+}
+
+// 2. テーブルをリネームし、疑似的にテーブルが削除された状態にする
+$table_prefix = ( new Prefix() )->tableNamePrefix();
+// $table_prefix で開始するテーブル名をすべて取得
+$table_names = $wpdb->get_col( "SHOW TABLES LIKE '{$table_prefix}%'" );
+foreach ( $table_names as $table_name ) {
+	// テーブル名をリネーム
+	$wpdb->query( "RENAME TABLE `{$table_name}` TO `bak_{$table_name}`" );
+}
+
+// 3. マイグレーションを実行して、テーブルを再生成する
+( new DatabaseMigrationService( $wpdb, new Environment() ) )->migrate( null );
+
+// 4. 署名用ウォレットデータの内容をリネームしたテーブルへ移動
+$server_signer_table_name = ( new TableName() )->serverSigner();
+$result                   = $wpdb->query( "INSERT INTO `bak_{$server_signer_table_name}` SELECT * FROM `{$server_signer_table_name}`" );
+if ( false === $result ) {
+	throw new \RuntimeException( '[D91480C9] Failed to copy server signer data: ' . $wpdb->last_error );
+}
+
+// 5. マイグレーションで作成されたテーブルをすべて削除し、バックアップのテーブルを元に戻す
+$table_names = $wpdb->get_col( "SHOW TABLES LIKE '{$table_prefix}%'" );
+foreach ( $table_names as $table_name ) {
+	$wpdb->query( "DROP TABLE IF EXISTS `{$table_name}`;" );
+	$wpdb->query( "RENAME TABLE `bak_{$table_name}` TO `{$table_name}`;" );
+}
+
+// 新しく生成された署名用ウォレットアドレスを取得
+$newAddress = $server_signer_service->getServerSigner()->address();
 echo "Server Signer Data has been reset.\n";
 echo "Old Address: $prevAddress\n";
 echo "New Address: $newAddress\n";
-
-// テーブルを再作成することで履歴を削除
-( new UnlockPaywallTransactionTable( $wpdb ) )->drop();
-( new UnlockPaywallTransactionTable( $wpdb ) )->create();
-( new UnlockPaywallTransferEventTable( $wpdb ) )->drop();
-( new UnlockPaywallTransferEventTable( $wpdb ) )->create();
-( new InvoiceTable( $wpdb ) )->drop();
-( new InvoiceTable( $wpdb ) )->create();
