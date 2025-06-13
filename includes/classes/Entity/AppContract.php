1032e44ace18cad3820a5e5e3e9edbd4e53f2d5d
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-13 00:40:10 +0000

    add: AppContractのプロパティを追加
    - activation_block_number
    - crawled_block_number

--- includes/classes/Entity/AppContract.php
+++ includes/classes/Entity/AppContract.php
@@ -4,20 +4,31 @@ declare(strict_types=1);
 namespace Cornix\Serendipity\Core\Entity;
 
 use Cornix\Serendipity\Core\ValueObject\Address;
+use Cornix\Serendipity\Core\ValueObject\BlockNumber;
 
 class AppContract {
-	public function __construct( Chain $chain, Address $address ) {
-		$this->chain   = $chain;
-		$this->address = $address;
+	public function __construct( Chain $chain, Address $address, ?BlockNumber $activation_block_number = null, ?BlockNumber $crawled_block_number = null ) {
+		$this->chain                   = $chain;
+		$this->address                 = $address;
+		$this->activation_block_number = $activation_block_number;
+		$this->crawled_block_number    = $crawled_block_number;
 	}
 
 	private Chain $chain;
 	private Address $address;
+	private ?BlockNumber $activation_block_number;
+	private ?BlockNumber $crawled_block_number;
 
 	public function chain(): Chain {
 		return $this->chain;
 	}
 	public function address(): Address {
 		return $this->address;
+	}
+	public function activationBlockNumber(): ?BlockNumber {
+		return $this->activation_block_number;
+	}
+	public function crawledBlockNumber(): ?BlockNumber {
+		return $this->crawled_block_number;
 	}
 }
