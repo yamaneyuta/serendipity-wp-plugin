5e50d58b7f11777206e84c86e2216740b6c8933f
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-13 01:22:55 +0000

    add: AppContractImpl

---
+++ includes/classes/Infrastructure/Database/Entity/AppContractImpl.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Core\Infrastructure\Database\Entity;
+
+use Cornix\Serendipity\Core\Entity\AppContract;
+use Cornix\Serendipity\Core\Entity\Chain;
+use Cornix\Serendipity\Core\Infrastructure\Database\ValueObject\AppContractTableRecord;
+use Cornix\Serendipity\Core\ValueObject\Address;
+use Cornix\Serendipity\Core\ValueObject\BlockNumber;
+
+class AppContractImpl extends AppContract {
+
+	private function __construct(
+		Chain $chain,
+		Address $address,
+		BlockNumber $activation_block_number,
+		BlockNumber $crawled_block_number
+	) {
+		parent::__construct( $chain, $address, $activation_block_number, $crawled_block_number );
+	}
+
+	public static function fromTableRecord( Chain $chain, AppContractTableRecord $record ): self {
+		return new self(
+			$chain,
+			Address::from( $record->address() ),
+			BlockNumber::from( $record->activationBlockNumber() ),
+			BlockNumber::from( $record->crawledBlockNumber() )
+		);
+	}
+}
