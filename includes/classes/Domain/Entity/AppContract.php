bb68b15f1d40e8728c14cfe691d34b15e400b39b
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-13 04:18:19 +0000

    chore: AppContractのコンストラクタをpublicからprotectedに変更

--- includes/classes/Domain/Entity/AppContract.php
+++ includes/classes/Domain/Entity/AppContract.php
@@ -8,7 +8,7 @@ use Cornix\Serendipity\Core\ValueObject\Address;
 use Cornix\Serendipity\Core\ValueObject\BlockNumber;
 
 class AppContract {
-	public function __construct( Chain $chain, Address $address, ?BlockNumber $activation_block_number = null, ?BlockNumber $crawled_block_number = null ) {
+	protected function __construct( Chain $chain, Address $address, ?BlockNumber $activation_block_number = null, ?BlockNumber $crawled_block_number = null ) {
 		$this->chain                   = $chain;
 		$this->address                 = $address;
 		$this->activation_block_number = $activation_block_number;
