d17944ccf013320a2d9bab71696c648a0eff3ee2
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-14 16:28:18 +0000

    chore: 未使用メソッドを削除

--- includes/classes/Infrastructure/Database/TableGateway/ChainTable.php
+++ includes/classes/Infrastructure/Database/TableGateway/ChainTable.php
@@ -3,9 +3,7 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Infrastructure\Database\TableGateway;
 
-use Cornix\Serendipity\Core\Constant\Config;
 use Cornix\Serendipity\Core\Domain\Entity\Chain;
-use Cornix\Serendipity\Core\Lib\Security\Validate;
 use Cornix\Serendipity\Core\Repository\Name\TableName;
 use Cornix\Serendipity\Core\Infrastructure\Database\ValueObject\ChainTableRecord;
 
@@ -22,7 +20,6 @@ class ChainTable extends TableBase {
 	 * @return ChainTableRecord[]
 	 */
 	public function all(): array {
-		// レコード数は少ないのですべてのレコードを取得してからフィルタリングする
 		$sql     = <<<SQL
 			SELECT `chain_id`, `name`, `rpc_url`, `confirmations`
 			FROM `{$this->tableName()}`
@@ -67,78 +64,6 @@ class ChainTable extends TableBase {
 		$result = $this->wpdb()->query( $sql );
 		if ( false === $result ) {
 			throw new \Exception( '[E01C7DE3] Failed to insert or update chain data. ' . $this->wpdb()->last_error );
-		}
-	}
-
-	/**
-	 * チェーン情報を新規作成します
-	 *
-	 * @param int    $chain_ID
-	 * @param string $name
-	 * @deprecated
-	 */
-	public function insert( int $chain_ID, string $name ) {
-		$this->wpdb()->insert(
-			$this->tableName(),
-			array(
-				'chain_id'      => $chain_ID,
-				'name'          => $name,
-				'rpc_url'       => null, // 初期値はnull
-				'confirmations' => (string) Config::MIN_CONFIRMATIONS, // 初期値は最小待機ブロック数
-			),
-			array( '%d', '%s', '%s', '%s', '%s' )
-		);
-		if ( $this->wpdb()->last_error ) {
-			throw new \Exception( '[E8B777B8] Failed to insert or update chain data. ' . $this->wpdb()->last_error );
-		}
-	}
-
-	/** @deprecated */
-	public function updateRpcURL( int $chain_ID, ?string $rpc_url ): void {
-		( ! is_null( $rpc_url ) ) && Validate::checkURL( $rpc_url );
-
-		$result = $this->wpdb()->update(
-			$this->tableName(),                 // table
-			array( 'rpc_url' => $rpc_url ),     // data
-			array( 'chain_id' => $chain_ID ),   // where
-			array( '%s' ),                      // format
-			array( '%d' )                       // where_format
-		);
-
-		if ( 1 < $result ) {
-			throw new \Exception( '[8314C8C0] Failed to update RPC URL. result: ' . var_export( $result, true ) );
-		}
-		if ( $this->wpdb()->last_error ) {
-			throw new \Exception( '[BD9BA6FD] Failed to update RPC URL. ' . $this->wpdb()->last_error );
-		}
-	}
-
-	/**
-	 * 指定されたチェーンIDの待機ブロック数を更新します。
-	 *
-	 * @param int        $chain_ID
-	 * @param int|string $confirmations
-	 * @deprecated
-	 */
-	public function updateConfirmations( int $chain_ID, $confirmations ): void {
-		Validate::checkConfirmations( $confirmations );
-
-		// confirmationsがint型の場合は文字列に変換
-		$confirmations = is_int( $confirmations ) ? (string) $confirmations : $confirmations;
-
-		$result = $this->wpdb()->update(
-			$this->tableName(),                 // table
-			array( 'confirmations' => $confirmations ), // data
-			array( 'chain_id' => $chain_ID ),   // where
-			array( '%s' ),                      // format
-			array( '%d' )                       // where_format
-		);
-
-		if ( 1 < $result ) {
-			throw new \Exception( '[7B341BB3] Failed to update confirmations. result: ' . var_export( $result, true ) );
-		}
-		if ( $this->wpdb()->last_error ) {
-			throw new \Exception( '[584805B9] Failed to update confirmations. ' . $this->wpdb()->last_error );
 		}
 	}
 }
