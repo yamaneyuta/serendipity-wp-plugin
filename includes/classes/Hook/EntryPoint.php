3fe6f89224d586d2a7508c30e5b9b6f499b4adff
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-15 06:20:45 +0000

    move: 各画面のHook処理を行うクラス

--- includes/classes/Hook/EntryPoint.php
+++ includes/classes/Hook/EntryPoint.php
@@ -2,14 +2,14 @@
 declare(strict_types=1);
 namespace Cornix\Serendipity\Core\Hook;
 
-use Cornix\Serendipity\Core\Hook\Page\AdminPageHook;
 use Cornix\Serendipity\Core\Hook\Cron\CronHook;
-use Cornix\Serendipity\Core\Hook\Page\PostEditHook;
-use Cornix\Serendipity\Core\Hook\Page\ViewPageHook;
 use Cornix\Serendipity\Core\Hook\Post\ContentIoHook;
 use Cornix\Serendipity\Core\Hook\Update\PluginUpdateHook;
 use Cornix\Serendipity\Core\Lib\Rest\RestProperty;
+use Cornix\Serendipity\Core\Presentation\AdminPageHook;
 use Cornix\Serendipity\Core\Presentation\GraphQLHook;
+use Cornix\Serendipity\Core\Presentation\PostEditHook;
+use Cornix\Serendipity\Core\Presentation\ViewPageHook;
 
 /**
  * フック登録のエントリーポイント
--- includes/classes/Presentation/ViewPageHook.php
+++ includes/classes/Presentation/ViewPageHook.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-namespace Cornix\Serendipity\Core\Hook\Page;
+namespace Cornix\Serendipity\Core\Presentation;
 
 use Cornix\Serendipity\Core\Features\Page\PhpVer;
 use Cornix\Serendipity\Core\Lib\Path\ProjectFile;
