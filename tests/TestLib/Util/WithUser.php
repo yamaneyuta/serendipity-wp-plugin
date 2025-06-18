b0f689cdb79688a1fdd8d14ae63561cce0f3caad
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-18 14:41:06 +0000

    add: WithUser
    生成されたインスタンスが有効な期間だけ指定されたユーザーで処理を行うクラス

---
+++ tests/TestLib/Util/WithUser.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Cornix\Serendipity\Test\Util;
+
+use Cornix\Serendipity\Test\Entity\WpUser;
+
+/**
+ * 指定したユーザーに切り替えるためのユーティリティクラス
+ *
+ * インスタンスが生成されると、コンストラクタで指定されたユーザーに切り替わります。
+ * インスタンスが破棄されると、元のユーザーに戻ります。
+ *
+ * ※ C++のstd::lock_guardのような使い方を想定しています。
+ */
+class WithUser {
+
+	public function __construct( WpUser $user ) {
+		// インスタンス生成時に指定されたユーザーに切り替える
+		$this->prev_user = WpUser::current();
+		$user->setCurrent();
+	}
+
+	public function __destruct() {
+		// コンストラクタで設定したユーザーに戻す
+		$this->prev_user->setCurrent();
+	}
+
+	private WpUser $prev_user;
+}
