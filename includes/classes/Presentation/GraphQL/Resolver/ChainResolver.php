0c909814b94e21ab2a0086ba58c5a586a0889f44
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-19 02:09:27 +0000

    refactor: ChainResolverをUseCaseを使用した形に書き換え

--- includes/classes/Presentation/GraphQL/Resolver/ChainResolver.php
+++ includes/classes/Presentation/GraphQL/Resolver/ChainResolver.php
@@ -3,29 +3,27 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Presentation\GraphQL\Resolver;
 
-use Cornix\Serendipity\Core\Application\Service\ChainService;
-use Cornix\Serendipity\Core\Domain\Entity\Token;
-use Cornix\Serendipity\Core\Domain\Repository\AppContractRepository;
-use Cornix\Serendipity\Core\Domain\Repository\TokenRepository;
-use Cornix\Serendipity\Core\Domain\Specification\TokensFilter;
+use Cornix\Serendipity\Core\Application\Dto\TokenDto;
+use Cornix\Serendipity\Core\Application\UseCase\GetAppContract;
+use Cornix\Serendipity\Core\Application\UseCase\GetChain;
+use Cornix\Serendipity\Core\Application\UseCase\GetTokensByChainId;
 use Cornix\Serendipity\Core\Lib\Security\Validate;
-use Cornix\Serendipity\Core\Domain\ValueObject\ChainID;
 
 class ChainResolver extends ResolverBase {
 
 	public function __construct(
-		AppContractRepository $app_contract_repository,
-		TokenRepository $token_repository,
-		ChainService $chain_service
+		GetChain $get_chain,
+		GetAppContract $get_app_contract,
+		GetTokensByChainId $get_tokens_by_chain_id
 	) {
-		$this->app_contract_repository = $app_contract_repository;
-		$this->token_repository        = $token_repository;
-		$this->chain_service           = $chain_service;
+		$this->get_chain              = $get_chain;
+		$this->get_app_contract       = $get_app_contract;
+		$this->get_tokens_by_chain_id = $get_tokens_by_chain_id;
 	}
 
-	private AppContractRepository $app_contract_repository;
-	private TokenRepository $token_repository;
-	private ChainService $chain_service;
+	private GetChain $get_chain;
+	private GetAppContract $get_app_contract;
+	private GetTokensByChainId $get_tokens_by_chain_id;
 
 	/**
 	 * #[\Override]
@@ -33,36 +31,36 @@ class ChainResolver extends ResolverBase {
 	 * @return array
 	 */
 	public function resolve( array $root_value, array $args ) {
-		$chain_ID = new ChainID( $args['chainID'] );
+		/** @var int */
+		$chain_id = $args['chainID'];
 
-		$chain = $this->chain_service->getChain( $chain_ID );
+		$chain = $this->get_chain->handle( $chain_id );
 
 		if ( is_null( $chain ) ) {
-			throw new \InvalidArgumentException( '[CA31D9B5] chain data is not found. chain id: ' . $chain_ID );
+			throw new \InvalidArgumentException( '[CA31D9B5] chain data is not found. chain id: ' . $chain_id );
 		}
 
 		// `AppContractResolver`の作成を省略してコールバックを定義
 		// `AppContractResolver`を作成した場合はここの処理を書き換えること。
 		$app_contract_callback = function () use ( $chain ) {
 			// 権限チェック不要
-			$app_contract = $this->app_contract_repository->get( $chain->id() );
-			$address      = is_null( $app_contract ) ? null : $app_contract->address();
-			return is_null( $address ) ? null : array( 'address' => $address->value() );
+			$app_contract = $this->get_app_contract->handle( $chain->id() );
+			$address      = null !== $app_contract ? $app_contract->address() : null;
+			return is_null( $address ) ? null : array( 'address' => $address );
 		};
 
-		$tokens_callback = function () use ( $root_value, $chain ) {
+		$tokens_callback = function () use ( $root_value, $chain_id ) {
 			Validate::checkHasAdminRole(); // 管理者権限が必要
 
-			$tokens_filter = ( new TokensFilter() )->byChainID( $chain->id() );
-			$tokens        = $tokens_filter->apply( $this->token_repository->all() );
+			$tokens = $this->get_tokens_by_chain_id->handle( $chain_id );
 
 			return array_map(
-				function ( Token $token ) use ( $root_value ) {
+				function ( TokenDto $token ) use ( $root_value, $chain_id ) {
 					return $root_value['token'](
 						$root_value,
 						array(
-							'chainID' => $token->chainID()->value(),
-							'address' => $token->address()->value(),
+							'chainID' => $chain_id,
+							'address' => $token->address(),
 						)
 					);
 				},
@@ -76,16 +74,16 @@ class ChainResolver extends ResolverBase {
 			return $root_value['networkCategory'](
 				$root_value,
 				array(
-					'networkCategoryID' => $chain->networkCategoryID()->value(),
+					'networkCategoryID' => $chain->networkCategoryId(),
 				)
 			);
 		};
 
 		return array(
-			'id'              => $chain->id()->value(),
+			'id'              => $chain->id(),
 			'appContract'     => $app_contract_callback,
-			'confirmations'   => (string) $chain->confirmations(),    // string型にして返す(GraphQLの定義した型に変換)
-			'rpcURL'          => $chain->rpcURL(),
+			'confirmations'   => $chain->confirmations(),
+			'rpcURL'          => $chain->rpcUrl(),
 			'tokens'          => $tokens_callback,
 			'networkCategory' => $network_category_callback,
 		);
