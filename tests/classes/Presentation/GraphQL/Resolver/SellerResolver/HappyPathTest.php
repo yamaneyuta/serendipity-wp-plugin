809dd17d59b08c223eba04efad714e4e908ab4c8
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-20 09:18:38 +0000

    move: SellerResolverのテストファイルを移動

--- tests/classes/Presentation/GraphQL/Resolver/SellerResolver/HappyPathTest.php
+++ tests/classes/Presentation/GraphQL/Resolver/SellerResolver/HappyPathTest.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Cornix\Serendipity\TestCase\Features\GraphQL\Resolver\SellerResolverTest;
+namespace Cornix\Serendipity\TestCase\Presentation\GraphQL\Resolver\SellerResolver;
 
 use Cornix\Serendipity\Core\Domain\ValueObject\Signature;
 use Cornix\Serendipity\Core\Domain\ValueObject\SigningMessage;
