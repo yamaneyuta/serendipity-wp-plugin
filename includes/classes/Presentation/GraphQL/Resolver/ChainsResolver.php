11e3437aa3dbb0db53953d0465f6b2453f8cfed7
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-20 01:59:05 +0000

    refactor: ChainsResolverでGetChainsByFilterを使用するように変更

--- includes/classes/Presentation/GraphQL/Resolver/ChainsResolver.php
+++ includes/classes/Presentation/GraphQL/Resolver/ChainsResolver.php
@@ -3,52 +3,36 @@ declare(strict_types=1);
 
 namespace Cornix\Serendipity\Core\Presentation\GraphQL\Resolver;
 
-use Cornix\Serendipity\Core\Application\Service\ChainService;
-use Cornix\Serendipity\Core\Domain\Specification\ChainsFilter;
+use Cornix\Serendipity\Core\Application\UseCase\GetChainsByFilter;
 use Cornix\Serendipity\Core\Lib\Security\Validate;
-use Cornix\Serendipity\Core\Domain\ValueObject\ChainID;
 
 class ChainsResolver extends ResolverBase {
 
-	public function __construct( ChainService $chain_service ) {
-		$this->chain_service = $chain_service;
+	public function __construct( GetChainsByFilter $get_chains_by_filter ) {
+		$this->get_chains_by_filter = $get_chains_by_filter;
 	}
-	private ChainService $chain_service;
+
+	private GetChainsByFilter $get_chains_by_filter;
 
 	/**
 	 * チェーン一覧を取得します。
 	 *
-	 * #[\Override]
-	 *
+	 * @inheritdoc
 	 * @return array
 	 */
-	public function resolve( array $root_value, array $args ) {
+	public function resolve( array $root_value, array $args ): array {
 		Validate::checkHasAdminRole();  // 管理者権限が必要
 
-		$filter          = $args['filter'] ?? null;
-		$filter_chain_ID = ChainID::fromNullableValue( $filter['chainID'] ?? null );
-		/** @var bool|null */
-		$filter_is_connectable = $filter['isConnectable'] ?? null;
-
-		// フィルタ処理
-		$chains_filter = new ChainsFilter();
-		// チェーンIDでフィルタ
-		if ( null !== $filter_chain_ID ) {
-			$chains_filter = $chains_filter->byChainID( $filter_chain_ID );
-		}
-		// 接続可能なチェーンでフィルタ
-		if ( null !== $filter_is_connectable ) {
-			$chains_filter = $chains_filter->byConnectable( $filter_is_connectable );
-		}
-
-		// フィルタを適用したチェーン一覧を取得
-		$chains = $chains_filter->apply( $this->chain_service->getAllChains() );
+		$chains = $this->get_chains_by_filter->handle(
+			$args['filter']['chainID'],
+			$args['filter']['isConnectable']
+		);
 
 		return array_map(
 			fn( $chain ) => $root_value['chain'](
 				$root_value,
 				array(
-					'chainID' => $chain->id()->value(),
+					'chainID' => $chain->id(),
 				)
 			),
 			$chains
