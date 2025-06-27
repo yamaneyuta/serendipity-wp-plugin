7539a350c49f485985d49b37720a5a85624f9882
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-06-27 11:49:36 +0000

    move: Privatenet及びBlockExplorerのDockerfileを移動

--- infra/block-explorer/entrypoint.sh
+++ infra/block-explorer/entrypoint.sh
@@ -3,13 +3,13 @@
 # `block-explorer`ディレクトリが存在しない場合はclone
 # 存在する場合は最新の状態に更新
 if [ ! -d block-explorer ]; then
-  echo "clone repository"
-  git clone https://github.com/Shaivpidadi/block-explorer
-  cd block-explorer
+	echo "clone repository"
+	git clone https://github.com/Shaivpidadi/block-explorer
+	cd block-explorer
 else
 	echo "update repository"
-  cd block-explorer
-  # git pull
+	cd block-explorer
+	# git pull
 	git fetch origin main
 	git reset --hard origin/main
 fi
