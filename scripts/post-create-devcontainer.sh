82cb11dfca3ef76aef24a89205f934222c307852
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-07-03 11:32:00 +0000

    fix(root): CIでは/workspacesではないのでgit cloneされたディレクトリを環境変数から取得

--- scripts/post-create-devcontainer.sh
+++ scripts/post-create-devcontainer.sh
@@ -2,22 +2,36 @@
 # devcontainer.json#postCreateCommand で指定されたスクリプト
 # ディレクトリ移動があるため、bash -e で実行すること
 
-# プロジェクトルートはこのスクリプトがあるディレクトリの親ディレクトリとする
-PROJECT_ROOT="$(dirname "$(dirname "$0")")"
-# ワークスペースのルートディレクトリ
-WORKSPACES=/workspaces
+# 以下の変数を初期化します
+# ※ このリポジトリをsubmoduleとして利用する場合、`PUBLIC_MONOREPO_ROOT`と`REPOSITORY_ROOT`は異なる値になります。
+# - PUBLIC_MONOREPO_ROOT ... モノレポのルートディレクトリ
+# - REPOSITORY_ROOT ... リポジトリがcloneされたディレクトリ
+init_variables() {
+    # PUBLIC_MONOREPO_ROOT 初期化
+    PUBLIC_MONOREPO_ROOT="$(dirname "$(dirname "$0")")"
+    
+    # REPOSITORY_ROOT 初期化
+    if [ -n "${GITHUB_WORKSPACE:-}" ]; then
+        REPOSITORY_ROOT="$GITHUB_WORKSPACE"
+    elif [ -n "${PWD:-}" ]; then
+        REPOSITORY_ROOT="$PWD"
+    else
+        echo "❌ [6D4AE551] Error: Neither GITHUB_WORKSPACE nor PWD is set." >&2
+        exit 1
+    fi
+}
 
-# プロジェクトが格納されるディレクトリ以下のパーミッションをログインユーザーに変更します
+# パーミッションをログインユーザーに変更します
 fix_permissions() {
     echo "🔑 [6A078A0F] Fixing permissions for current directory..."
-    sudo chown -R "$(whoami):$(whoami)" "$WORKSPACES"
+    sudo chown -R "$(whoami):$(whoami)" "$REPOSITORY_ROOT"
     echo "✅ [8FC2C6C7] Permissions fixed successfully."
 }
 
 # NPMパッケージをインストールします
 install_npm_packages() {
     echo "📦 [8E678580] Installing npm packages..."
-    cd $WORKSPACES
+    cd $REPOSITORY_ROOT
     pnpm install --frozen-lockfile
     echo "✅ [2B4D55B0] NPM packages installed successfully."
     cd -
@@ -25,7 +39,7 @@ install_npm_packages() {
 
 install_php_packages() {
     echo "📦 [E1739CA3] Installing PHP packages..."
-    cd "$PROJECT_ROOT/apps/wp-plugin"
+    cd "$PUBLIC_MONOREPO_ROOT/apps/wp-plugin"
     bash .bin/install-php-packages.sh
     bash .bin/install-intelephense-includes.sh
     echo "✅ [9A0F8FF5] PHP packages installed successfully."
@@ -35,7 +49,7 @@ install_php_packages() {
 build() {
     # アプリケーションのビルドを実行
     echo "🏗️ [562FF5A7] Building application..."
-    cd $WORKSPACES
+    cd $REPOSITORY_ROOT
     npm run build
     echo "✅ [45DF7646] Application built successfully."
     cd -
@@ -44,9 +58,9 @@ build() {
 # メイン関数
 main() {
     echo "🚀 [268E63B5] Starting post-create script..."
-    
-    cd "$PROJECT_ROOT"
 
+    init_variables
+    
     fix_permissions
     install_npm_packages &
     NPM_INSTALL_PID=$!
