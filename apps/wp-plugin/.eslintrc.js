73451d95cf7bcbfcf58ceb2d7207348c205fc00d
山根 佑太 <yamaneyuta@users.noreply.github.com>
2025-07-04 06:12:59 +0000

    chore(config): モノレポのルートディレクトリにインストールされているパッケージも参照できるように設定を変更

--- apps/wp-plugin/.eslintrc.js
+++ apps/wp-plugin/.eslintrc.js
@@ -3,16 +3,4 @@ config.extends.push( 'plugin:storybook/recommended' );
 
 config.ignorePatterns = [ ...config.ignorePatterns, 'src/types/gql' ];
 
-// storybook関連のdevDependenciesをルートから参照することを許可
-config.rules = {
-	...config.rules,
-	'import/no-extraneous-dependencies': [
-		'error',
-		{
-			devDependencies: true,
-			packageDir: [ '.', '../..' ], // ワークスペースルートも検索対象に追加
-		},
-	],
-};
-
 module.exports = config;
--- packages/config/.eslintrc.js
+++ packages/config/.eslintrc.js
@@ -1,7 +1,7 @@
+// @ts-check
 const fs = require( 'fs' );
 const path = require( 'path' );
 
-/** @type {import('eslint').ESLint.ConfigData} */
 const config = require( '@wordpress/eslint-plugin/configs/recommended' );
 
 config.rules = {
@@ -39,6 +39,39 @@ config.settings[ 'import/resolver' ] = {
 	// `eslint-import-resolver-typescript`
 	// -> `import/named`を解決
 	typescript: {},
+};
+
+// `import/no-extraneous-dependencies`ルールの設定を追加
+// => pnpmのワークスペースを使用しているので、モノレポのディレクトリもpackageDirに追加する必要がある
+// ワークスペースルートも検索対象に追加するため、`pnpm-lock.yaml`ファイルが存在するディレクトリまでを検索する
+/** @type {string[]} */
+const noExtraneousDependenciesPackagesDir = ( () => {
+	// ルートディレクトリで実行した場合は、現在のディレクトリを返す
+	if ( fs.existsSync( path.join( process.cwd(), 'pnpm-lock.yaml' ) ) ) {
+		return [ '.' ];
+	}
+
+	// その他のディレクトリの場合は、親ディレクトリを辿っていき、`pnpm-lock.yaml`が存在するディレクトリを追加する
+	let dir = process.cwd();
+	const rootDir = path.parse( process.cwd() ).root;
+	while ( dir !== rootDir ) {
+		if ( fs.existsSync( path.join( dir, 'pnpm-lock.yaml' ) ) ) {
+			return [ '.', dir ];
+		}
+		dir = path.dirname( dir );
+	}
+	throw new Error( '[3611AE6E] pnpm-lock.yaml not found in any parent directory.' );
+} )();
+
+config.rules = {
+	...config.rules,
+	'import/no-extraneous-dependencies': [
+		'error',
+		{
+			devDependencies: true,
+			packageDir: noExtraneousDependenciesPackagesDir,
+		},
+	],
 };
 
 module.exports = config;
